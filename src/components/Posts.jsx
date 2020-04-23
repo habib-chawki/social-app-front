48ff2fdc60e64225b8020c787754b1c7f0029e1c
habib-chawki <touati.habib.chawki@gmail.com>
2020-04-23 21:41:59 +0200

    Fix: post content

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -2,23 +2,22 @@ import React, { useState } from 'react';
 import Post from './Post';
 
 function Posts() {
-   const [posts, setPosts] = useState([]);
-   const [postContent, setPostContent] = useState('');
+   const [posts, setPosts] = useState([]); // list of posts
+   const [content, setContent] = useState(''); // post content
 
    // handle adding new post
    const addPost = () => {
-      setPosts([...posts, { owner: 'me', content: 'This is a new post' }]);
+      setPosts([...posts, { owner: 'me', content }]);
    };
 
    // handle post content
    const handleChange = (event) => {
-      setPostContent(event.target.value);
-      console.log('postContent: ', postContent);
+      setContent(event.target.value);
    };
 
    return (
       <div>
-         <input type="text" value={postContent} onChange={handleChange} />
+         <input type="text" value={content} onChange={handleChange} />
          <button onClick={addPost}>Add post</button>
          {posts.map(({ owner, content }, index) => (
             <Post key={index} owner={owner} content={content} />
