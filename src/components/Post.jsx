5a9df3ed1ea2a1b212b2ab7c4ca8f2cf13579e0c
habib-chawki <touati.habib.chawki@gmail.com>
2020-04-23 21:38:40 +0200

    Handle post content

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 function Post({ owner, content }) {
    return (
+      // a post is defined with an owner and a content
       <div>
          <h2>{owner}</h2>
          <p>{content}</p>
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -3,13 +3,22 @@ import Post from './Post';
 
 function Posts() {
    const [posts, setPosts] = useState([]);
+   const [postContent, setPostContent] = useState('');
 
+   // handle adding new post
    const addPost = () => {
       setPosts([...posts, { owner: 'me', content: 'This is a new post' }]);
    };
 
+   // handle post content
+   const handleChange = (event) => {
+      setPostContent(event.target.value);
+      console.log('postContent: ', postContent);
+   };
+
    return (
       <div>
+         <input type="text" value={postContent} onChange={handleChange} />
          <button onClick={addPost}>Add post</button>
          {posts.map(({ owner, content }, index) => (
             <Post key={index} owner={owner} content={content} />
