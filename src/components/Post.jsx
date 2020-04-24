d807c34b01a76ce2e86875eecad99ee3e191814f
habib-chawki <touati.habib.chawki@gmail.com>
2020-04-24 19:23:56 +0200

    Fix: manage comments state within Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Post({ owner, content, addComment }) {
+function Post({ owner, content }) {
+   const [comments, setComments] = useState([]);
+
+   // handle adding new comment
+   const addComment = () => {
+      setComments([...comments, { content: 'new comment' }]);
+   };
+
    return (
       // a post is defined with an owner and a content
       <div>
@@ -8,6 +15,11 @@ function Post({ owner, content, addComment }) {
          <p>{content}</p>
          <input type="text" />
          <button onClick={addComment}>comment</button>
+         <ul>
+            {comments.map((comment, index) => (
+               <li key={index}>{comment.content}</li>
+            ))}
+         </ul>
       </div>
    );
 }
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -4,17 +4,10 @@ import Post from './Post';
 function Posts() {
    const [posts, setPosts] = useState([]); // list of posts
    const [content, setContent] = useState(''); // post content
-   const [comments, setComments] = useState([]); // list of comments
 
    // handle adding new post
    const addPost = () => {
       setPosts([...posts, { owner: 'me', content }]);
-   };
-
-   // handle adding new comment
-   const addComment = () => {
-      setComments([...comments, { content: 'new comment' }]);
-      console.log(comments);
    };
 
    // handle post content
@@ -27,12 +20,7 @@ function Posts() {
          <input type="text" value={content} onChange={handleChange} />
          <button onClick={addPost}>Add post</button>
          {posts.map(({ owner, content }, index) => (
-            <Post
-               key={index}
-               owner={owner}
-               content={content}
-               addComment={addComment}
-            />
+            <Post key={index} owner={owner} content={content} />
          ))}
       </div>
    );
