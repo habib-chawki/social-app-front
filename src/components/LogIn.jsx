98b163d6692ebdced151f6725de5c353e2dfa281
habib-chawki <touati.habib.chawki@gmail.com>
2020-04-27 19:28:48 +0200

    Handle email and password input

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function LogIn() {
+   const [email, setEmail] = useState('');
+   const [password, setPassword] = useState('');
+
+   const handleEmail = (event) => {
+      setEmail(event.target.value);
+      console.log(email);
+   };
+
+   const handlePassword = (event) => {
+      setPassword(event.target.value);
+      console.log(password);
+   };
+
    return (
       <form>
          <label htmlFor="email">Email:</label>
-         <input type="text" id="email" placeholder="Email..." />
+         <input
+            type="text"
+            value={email}
+            onChange={handleEmail}
+            id="email"
+            placeholder="Email..."
+         />
          <label htmlFor="password">Password:</label>
-         <input type="text" id="password" placeholder="Password..." />
+         <input
+            type="password"
+            value={password}
+            onChange={handlePassword}
+            id="password"
+            placeholder="Password..."
+         />
          <input type="submit" />
       </form>
    );
