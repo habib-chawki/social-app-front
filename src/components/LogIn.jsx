ed9277b0ce29d0dffa8ee3dbd009a957e8f2f0b5
habib-chawki <touati.habib.chawki@gmail.com>
2020-04-30 20:58:07 +0200

    Add login input validation

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -2,13 +2,23 @@ import React, { useState } from 'react';
 
 function LogIn() {
    // manage email and password state
-   const [credentials, setCredentials] = useState({ email: '', password: '' });
+   const [credentials, setCredentials] = useState({
+      email: '',
+      password: '',
+      errors: {},
+   });
 
    // handle email and password input
    const handleCredentials = (event) => {
+      const errors = {};
+      if (event.target.value.length < 3) {
+         errors[event.target.name] = `Invalid ${event.target.name}`;
+      }
+
       setCredentials({
          ...credentials,
          [event.target.name]: event.target.value,
+         errors,
       });
    };
 
@@ -31,6 +41,7 @@ function LogIn() {
             id="email"
             placeholder="Email..."
          />
+         {credentials.errors.email && <p>{credentials.errors.email}</p>}
          <label htmlFor="password">Password:</label>
          <input
             type="password"
@@ -40,6 +51,7 @@ function LogIn() {
             id="password"
             placeholder="Password..."
          />
+         {credentials.errors.password && <p>{credentials.errors.password}</p>}
          <input type="submit" value="Log in" />
       </form>
    );
