fc8471d5f931f0267b40e999de7b04234385e155
habib-chawki <touati.habib.chawki@gmail.com>
2020-05-08 23:56:03 +0200

    Setup routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import SignUp from './components/SignUp';
+import LogIn from './components/LogIn';
 
 function App() {
-   return <SignUp />;
+   return (
+      <div>
+         <Switch>
+            <Route exact path="/" component={SignUp} />
+            <Route path="/login" component={LogIn} />
+         </Switch>
+      </div>
+   );
 }
 
 export default App;
