ae471ab08d8ffaf24041e523137427dfb8b1e373
habib-chawki <touati.habib.chawki@gmail.com>
2020-05-09 00:00:02 +0200

    Setup links

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import CredentialsForm from './common/CredentialsForm';
 
 function LogIn() {
-   return <CredentialsForm title="Log in" />;
+   return (
+      <div>
+         <CredentialsForm title="Log in" />
+         <Link to="/">Signup</Link>
+      </div>
+   );
 }
 
 export default LogIn;
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import CredentialsForm from './common/CredentialsForm';
 
 function SignUp() {
-   return <CredentialsForm title="Sign up" />;
+   return (
+      <div>
+         <CredentialsForm title="Sign up" />
+         <Link to="/login">LogIn</Link>
+      </div>
+   );
 }
 
 export default SignUp;
