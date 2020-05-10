5154a159a9e4bb15575091e060d822c0f4b03535
habib-chawki <touati.habib.chawki@gmail.com>
2020-05-10 18:08:25 +0200

    handle signup form submission

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import CredentialsForm from './common/CredentialsForm';
 
 function SignUp() {
-   const handleSignUp = () => {
-      console.log('Handle signup...');
+   // handle sign up form submission
+   const handleSignUp = async () => {
+      console.log('Handling signup...');
+
+      // user sign up
+      try {
+         const response = await axios.post(
+            'http://localhost:3001/user/signup',
+            {
+               email: 'habib@email.com',
+               password: 'P@$$W0rD',
+            }
+         );
+
+         console.log(response);
+      } catch (e) {
+         // Error
+         console.log(e.message);
+      }
    };
 
    return (
