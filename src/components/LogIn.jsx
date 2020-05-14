afd55695c848bb2633f55a85f665feebefadf059
habib-chawki <touati.habib.chawki@gmail.com>
2020-05-14 18:07:28 +0200

    Persist auth token to localStorage upon login

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -4,12 +4,17 @@ import { Link } from 'react-router-dom';
 import CredentialsForm from './common/CredentialsForm';
 import server from '../utils';
 
-function LogIn() {
+function LogIn({ history }) {
    const handleLogIn = async ({ email, password }) => {
       // handle login form submission
       try {
          const response = await server.post('user/login/', { email, password });
-         console.log(response);
+
+         // persist auth token to localStorage
+         localStorage.setItem('Token', response.data.token);
+
+         // navigate user to posts
+         history.replace('/posts');
       } catch (e) {
          console.log(e.message);
       }
