645e9919e711266d0b267dd89af72d410854c092
habib-chawki <touati.habib.chawki@gmail.com>
2020-05-18 22:13:33 +0200

    Persist posts to database

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,14 +1,33 @@
 import React, { useState } from 'react';
 import Post from './Post';
+import server from '../utils/server';
 
 function Posts() {
    const [posts, setPosts] = useState([]); // list of posts
    const [content, setContent] = useState(''); // post content
 
    // handle adding new post
-   const addPost = () => {
-      setPosts([{ owner: 'me', content }, ...posts]);
-      setContent('');
+   const addPost = async () => {
+      try {
+         // retrieve auth token from localStorage
+         const token = 'Bearer ' + localStorage.getItem('Token');
+
+         // send post request with authotization header
+         const response = await server({
+            url: '/post',
+            method: 'post',
+            data: { content },
+            headers: { authorization: token },
+         });
+
+         // update posts list
+         setPosts([{ id: response.data.id, owner: 'me', content }, ...posts]);
+         setContent('');
+
+         console.log(response);
+      } catch (e) {
+         console.log(e.message);
+      }
    };
 
    // handle post input change
@@ -20,8 +39,8 @@ function Posts() {
       <div>
          <input type="text" value={content} onChange={handleChange} />
          <button onClick={addPost}>Add post</button>
-         {posts.map(({ owner, content }, index) => (
-            <Post key={index} owner={owner} content={content} />
+         {posts.map(({ id, owner, content }) => (
+            <Post key={id} owner={owner} content={content} />
          ))}
       </div>
    );
