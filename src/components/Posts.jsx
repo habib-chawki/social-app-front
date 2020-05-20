381c631f7e26f04ba6aa0f84dff12bd27789923a
habib-chawki <touati.habib.chawki@gmail.com>
2020-05-20 21:50:47 +0200

    Render posts list when component mounts

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Post from './Post';
 import server from '../utils/server';
 
@@ -6,13 +6,32 @@ function Posts() {
    const [posts, setPosts] = useState([]); // list of posts
    const [content, setContent] = useState(''); // post content
 
+   // retrieve auth token from localStorage
+   const token = 'Bearer ' + localStorage.getItem('Token');
+
+   // render posts list when component mounts
+   useEffect(() => {
+      console.log('useEffect triggered ...');
+      const fetchPosts = async () => {
+         try {
+            const response = await server({
+               url: '/post/all',
+               method: 'get',
+               headers: { authorization: token },
+            });
+            console.log('response: ', response);
+         } catch (e) {
+            console.log('Unable to fetch posts list.' + e.message);
+         }
+      };
+
+      fetchPosts();
+   }, [posts, token]);
+
    // handle adding new post
    const addPost = async () => {
       try {
-         // retrieve auth token from localStorage
-         const token = 'Bearer ' + localStorage.getItem('Token');
-
-         // send post request with authotization header
+         // make api request to persist post in db
          const response = await server({
             url: '/post',
             method: 'post',
@@ -29,13 +48,13 @@ function Posts() {
    };
 
    // handle post input change
-   const handleChange = (event) => {
+   const handlePostInput = (event) => {
       setContent(event.target.value);
    };
 
    return (
       <div>
-         <input type="text" value={content} onChange={handleChange} />
+         <input type="text" value={content} onChange={handlePostInput} />
          <button onClick={addPost}>Add post</button>
          {posts.map(({ id, owner, content }) => (
             <Post key={id} owner={owner} content={content} />
