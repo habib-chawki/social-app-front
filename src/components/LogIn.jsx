ebe913d75ac46b307515ed479c00959170b5efdf
habib-chawki <touati.habib.chawki@gmail.com>
2020-06-02 22:40:12 +0200

    Refactor login component

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -2,28 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import CredentialsForm from './common/CredentialsForm';
-import server from '../utils/server';
-import { setToken } from '../services/token';
 
-function LogIn({ history }) {
-   // handle login form submission
-   const handleLogIn = async ({ email, password }) => {
-      try {
-         const response = await server.post('user/login/', { email, password });
-
-         // persist auth token to localStorage
-         setToken(response.data.token);
-
-         // navigate user to posts
-         history.replace('/posts');
-      } catch (e) {
-         console.log(e.message);
-      }
-   };
-
+function LogIn() {
    return (
       <div>
-         <CredentialsForm title="Log in" handleFormSubmission={handleLogIn} />
+         <CredentialsForm type="login" title="Log in" />
          <p>
             Not registered yet ? <Link to="/">Sign Up</Link>
          </p>
