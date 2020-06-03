5aa5fc3cfc491a00d024e6325b25e598f347bde6
habib-chawki <touati.habib.chawki@gmail.com>
2020-06-03 22:41:02 +0200

    Refactor server

--- src/utils/server.js
+++ src/utils/server.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
+import { getToken } from '../services/token';
 
-// setup base url
+// setup base url and get auth token
 const baseURL = 'http://localhost:3001';
+const authToken = getToken();
 
 const server = axios.create({
    baseURL,
+   headers: { authorization: authToken },
 });
 
 export default server;
