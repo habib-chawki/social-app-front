c9277b43bbf103e28a77c0a2668c018dfaf175a8
habib-chawki <touati.habib.chawki@gmail.com>
2020-06-08 22:02:55 +0200

    Setup user logout

--- src/services/user.js
+++ src/services/user.js
@@ -1,5 +1,5 @@
 import server from '../utils/server';
-import { setToken } from './token';
+import { setToken, removeToken } from './token';
 
 // handle signup / login form submission
 async function handleFormSubmission(formType, { email, password }) {
@@ -18,4 +18,19 @@ async function handleFormSubmission(formType, { email, password }) {
    }
 }
 
-export { handleFormSubmission };
+// handle user logout
+async function logoutUser() {
+   try {
+      await server({
+         url: 'user/logout',
+         method: 'post',
+      });
+
+      // remove token from localStorage
+      removeToken();
+   } catch (e) {
+      console.log('Unable to log user out: ' + e.message);
+   }
+}
+
+export { handleFormSubmission, logoutUser };
