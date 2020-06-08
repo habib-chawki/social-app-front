5381e70df70247f965742f8a844038696c1f78f0
habib-chawki <touati.habib.chawki@gmail.com>
2020-06-08 21:50:01 +0200

    Remove token from localStorage

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { removeToken } from '../../services/token';
+
 function Header() {
-   return <Link to="/">Log out</Link>;
+   return (
+      <Link to="/" onClick={removeToken}>
+         Log out
+      </Link>
+   );
 }
 
 export default Header;
