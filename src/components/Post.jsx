2887e226a63d8b1bac0e00f496a7f7e0afbdb45a
habib-chawki <touati.habib.chawki@gmail.com>
2020-06-28 18:24:56 +0200

    Refactor: cleanup

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,15 +1,7 @@
 import React, { useState } from 'react';
-
-import {
-   createComment,
-   updateComment,
-   deleteComment,
-} from '../services/comment';
+import Comments from './Comments';
 
 function Post({ onUpdatePost, onDeletePost, ...post }) {
-   const [comments, setComments] = useState(post.comments);
-   const [commentInput, setCommentInput] = useState('');
-
    const [editPost, setEditPost] = useState(false);
    const [editedPostInput, setEditedPostInput] = useState(post.content);
 
@@ -24,34 +16,6 @@ function Post({ onUpdatePost, onDeletePost, ...post }) {
    // handle delete post
    const handleDeletePost = () => {
       onDeletePost(post.id);
-   };
-
-   // keep track of comment input change
-   const handleCommentInput = (event) => {
-      setCommentInput(event.target.value);
-   };
-
-   // add new comment
-   const handleCreateComment = async () => {
-      const { _id, owner, comment } = await createComment(
-         post.id,
-         commentInput
-      );
-
-      // update comments list (push new comment)
-      setComments([...comments, { _id, owner, comment }]);
-      setCommentInput('');
-   };
-
-   // handle delete comment
-   const handleDeleteComment = (postId, commentId) => {
-      deleteComment(postId, commentId);
-      setComments(comments.filter((comment) => comment._id !== commentId));
-   };
-
-   // handle update comment
-   const handleUpdateComment = (postId, commentId) => {
-      updateComment(postId, commentId);
    };
 
    // render post content in a text <input> when editing or <p> otherwise
@@ -69,29 +33,6 @@ function Post({ onUpdatePost, onDeletePost, ...post }) {
       );
    };
 
-   // render list of comments
-   const renderComments = () => {
-      return (
-         <ul>
-            {comments.map((comment) => (
-               <li key={comment._id}>
-                  {comment.owner}: {comment.comment}
-                  <button
-                     onClick={() => handleDeleteComment(post.id, comment._id)}
-                  >
-                     delete
-                  </button>
-                  <button
-                     onClick={() => handleUpdateComment(post.id, comment._id)}
-                  >
-                     update
-                  </button>
-               </li>
-            ))}
-         </ul>
-      );
-   };
-
    return (
       <div>
          <h2>{post.owner}</h2>
@@ -100,10 +41,7 @@ function Post({ onUpdatePost, onDeletePost, ...post }) {
          <button onClick={handleDeletePost}>delete</button>
          <button onClick={handleUpdatePost}>update</button>
 
-         <input value={commentInput} onChange={handleCommentInput} />
-         <button onClick={handleCreateComment}>comment</button>
-
-         {renderComments()}
+         <Comments id={post.id} comments={post.comments} />
       </div>
    );
 }
