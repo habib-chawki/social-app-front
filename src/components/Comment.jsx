9b4ab69cbe420bb2f2d28946bf7f45d89c22be35
habib-chawki <touati.habib.chawki@gmail.com>
2020-06-28 18:23:57 +0200

    Create Comment component

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,5 +1,27 @@
 import React from 'react';
 
-function Comment() {}
+function Comment({ onUpdateComment, onDeleteComment, ...comment }) {
+   // handle deleting the comment
+   const handleDeleteComment = () => {
+      comment.onDeleteComment(comment.id);
+   };
+
+   // handle updating the comment
+   const handleUpdateComment = () => {
+      comment.onUpdateComment(comment.id);
+   };
+
+   return (
+      <li key={comment._id}>
+         {comment.owner}: {comment.content}
+         <button onClick={() => handleDeleteComment(comment._id)}>
+            delete
+         </button>
+         <button onClick={() => handleUpdateComment(comment._id)}>
+            update
+         </button>
+      </li>
+   );
+}
 
 export default Comment;
