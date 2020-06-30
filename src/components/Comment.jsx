16f43ac51ad2a966935642e5df262b87e416c605
habib-chawki <touati.habib.chawki@gmail.com>
2020-06-30 23:04:46 +0200

    Setup comments update

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Comment({ onUpdateComment, onDeleteComment, ...comment }) {
+   const [editComment, setEditComment] = useState(false);
+   const [editedCommentInput, setEditedCommentInput] = useState(
+      comment.content
+   );
+
+   // handle updating the comment
+   const handleUpdateComment = () => {
+      if (editComment) {
+         onUpdateComment(comment.id, editedCommentInput);
+      }
+      setEditComment(!editComment);
+   };
+
    // handle deleting the comment
    const handleDeleteComment = () => {
       onDeleteComment(comment.id);
    };
 
-   // handle updating the comment
-   const handleUpdateComment = () => {
-      onUpdateComment(comment.id);
+   const renderCommentContent = () => {
+      return editComment ? (
+         <input
+            type="text"
+            value={editedCommentInput}
+            onChange={(event) => {
+               setEditedCommentInput(event.target.value);
+            }}
+         />
+      ) : (
+         <p>{comment.content}</p>
+      );
    };
 
    return (
       <li key={comment.id}>
-         {comment.owner}: {comment.content}
+         <h4>{comment.owner}</h4>
+         {renderCommentContent()}
          <button onClick={handleDeleteComment}>delete</button>
          <button onClick={handleUpdateComment}>update</button>
       </li>
