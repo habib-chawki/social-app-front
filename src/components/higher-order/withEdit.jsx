c5093d3e1cf594bf90b8e76532cf7f7eb4c20dbd
habib-chawki <touati.habib.chawki@gmail.com>
2020-07-11 20:15:56 +0200

    Setup withEdit

--- src/components/higher-order/withEdit.jsx
+++ src/components/higher-order/withEdit.jsx
@@ -1,5 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function withEdit(Component) {}
+function withEdit(Component, content = '') {
+   return (props) => {
+      // const [editContent, setEditContent] = useState(false);
+      // const [editedContent, setEditedContent] = useState(content);
+
+      return (
+         <div>
+            <Component {...props} /> <p>hi commnet</p>
+         </div>
+      );
+   };
+}
 
 export default withEdit;
