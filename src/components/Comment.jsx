39e073c7b840565ddbd89fbc4c3ae1bd016651d1
habib-chawki <touati.habib.chawki@gmail.com>
2020-07-12 23:38:01 +0200

    Fix: use higher order component with edit

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,45 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import withEdit from './higher-order/withEdit';
 
 function Comment(comment) {
-   const [editComment, setEditComment] = useState(false);
-   const [editedCommentInput, setEditedCommentInput] = useState(
-      comment.content
-   );
-
-   // handle updating the comment
-   const handleUpdateComment = () => {
-      if (editComment) {
-         comment.onUpdate(comment.id, editedCommentInput);
-      }
-      setEditComment(!editComment);
-   };
-
    // handle deleting the comment
    const handleRemoveComment = () => {
       comment.onRemove(comment.id);
    };
 
-   const renderCommentContent = () => {
-      return editComment ? (
-         <input
-            type="text"
-            value={editedCommentInput}
-            onChange={(event) => {
-               setEditedCommentInput(event.target.value);
-            }}
-         />
-      ) : (
-         <p>{comment.content}</p>
-      );
-   };
-
    return (
       <li key={comment.id}>
          <h4>{comment.owner}</h4>
-         {renderCommentContent()}
+
+         {comment.renderContent()}
+
          <button onClick={handleRemoveComment}>delete</button>
-         <button onClick={handleUpdateComment}>update</button>
+         <button onClick={() => comment.handleUpdate()}>update</button>
       </li>
    );
 }
