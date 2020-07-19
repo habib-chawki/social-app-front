ce77db40e547300aecd212984a355214b10f1569
habib-chawki <touati.habib.chawki@gmail.com>
2020-07-19 13:34:04 +0200

    Refactor: remove comment

--- src/components/custom/Comment.jsx
+++ src/components/custom/Comment.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
+
 import withEdit from '../higher-order/withEdit';
 
 function Comment(comment) {
-   // handle deleting the comment
-   const handleRemoveComment = () => {
-      comment.onRemove(comment.id);
-   };
-
    return (
       <li key={comment.id}>
          <h4>{comment.owner}</h4>
 
          {comment.renderContent()}
 
-         <button onClick={handleRemoveComment}>delete</button>
+         <button onClick={() => comment.handleRemove()}>delete</button>
          <button onClick={() => comment.handleUpdate()}>update</button>
       </li>
    );
