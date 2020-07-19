4e9ce94c9d747839a3fbf348cc32349c62c16460
habib-chawki <touati.habib.chawki@gmail.com>
2020-07-19 13:34:17 +0200

    Refactor: remove post

--- src/components/custom/Post.jsx
+++ src/components/custom/Post.jsx
@@ -4,18 +4,13 @@ import Comments from './Comments';
 import withEdit from '../higher-order/withEdit';
 
 function Post(post) {
-   // delete post
-   const handleRemovePost = () => {
-      post.onRemove(post.id);
-   };
-
    return (
       <div>
          <h2>{post.owner}</h2>
 
          {post.renderContent()}
 
-         <button onClick={handleRemovePost}>delete</button>
+         <button onClick={() => post.handleRemove()}>delete</button>
          <button onClick={() => post.handleUpdate()}>update</button>
 
          <Comments id={post.id} comments={post.comments} />
