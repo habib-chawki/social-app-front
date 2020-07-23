cca779c780d59dfa8569329980f84bb958979ecc
habib-chawki <touati.habib.chawki@gmail.com>
2020-07-23 10:05:55 +0200

    Feat: update user profile

--- src/services/profile.js
+++ src/services/profile.js
@@ -13,6 +13,19 @@ async function fetchProfile() {
       console.log('Unable to fetch user profile ' + e.message);
    }
 }
-function updateProfile() {}
+
+async function updateProfile(profile) {
+   try {
+      const response = await server({
+         url: '/profile',
+         method: 'put',
+         data: profile,
+      });
+
+      return response.data;
+   } catch (e) {
+      console.log('Unable to update profile ' + e.message);
+   }
+}
 
 export { fetchProfile, updateProfile };
