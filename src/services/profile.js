c979c310c7c98f4c8fe1e3773c54b22061f56733
habib-chawki <touati.habib.chawki@gmail.com>
2020-07-23 10:01:58 +0200

    Feat: fetch user profile

--- src/services/profile.js
+++ src/services/profile.js
@@ -1,6 +1,18 @@
-// import server from '../utils/server';
+import server from '../utils/server';
 
-function fetch() {}
-function update() {}
+async function fetchProfile() {
+   try {
+      const response = await server({
+         url: '/profile',
+         method: 'get',
+      });
 
-export { fetch, update };
+      // return user profile
+      return response.data;
+   } catch (e) {
+      console.log('Unable to fetch user profile ' + e.message);
+   }
+}
+function updateProfile() {}
+
+export { fetchProfile, updateProfile };
