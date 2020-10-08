d7795f78d51163878d504b0b0a0e010475dfa880
habib-chawki <touati.habib.chawki@gmail.com>
2020-10-08 13:20:57 +0200

    Signup form styling

--- src/components/custom/SignUp.jsx
+++ src/components/custom/SignUp.jsx
@@ -1,26 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import Button from '@material-ui/core/Button';
+import { Button, Box } from '@material-ui/core';
 
 import { signupUser } from '../../services/user';
 import withValidation from '../higher-order/withValidation';
 
 function SignUp({ renderInput, handleSubmit }) {
    return (
-      <div>
-         <h1>Sign Up</h1>
-         <form onSubmit={handleSubmit}>
-            {renderInput({ type: 'text', name: 'email' })}
-            {renderInput({ type: 'password', name: 'password' })}
-            <Button type="submit" variant="contained" color="primary">
-               Sign up
-            </Button>
-         </form>
-         <p>
-            Already registered ? <Link to="/login">Log In</Link>
-         </p>
-      </div>
+      <Box
+         display="flex"
+         flexDirection="row"
+         justifyContent="center"
+         bgcolor="lightblue"
+      >
+         <Box display="flex" flexDirection="column">
+            <h1>Sign Up</h1>
+            <form onSubmit={handleSubmit}>
+               {renderInput({ type: 'text', name: 'email' })}
+               {renderInput({ type: 'password', name: 'password' })}
+               <Button type="submit" variant="contained" color="primary">
+                  Sign up
+               </Button>
+            </form>
+            <p>
+               Already registered ? <Link to="/login">Log In</Link>
+            </p>
+         </Box>
+      </Box>
    );
 }
 
