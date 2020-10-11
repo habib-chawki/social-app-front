bc338c21e9023583f79783be3bdbc534f8460b65
habib-chawki <touati.habib.chawki@gmail.com>
2020-10-11 11:25:52 +0200

    Refactor: signup form

--- src/components/custom/SignUp.jsx
+++ src/components/custom/SignUp.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { Button, Box } from '@material-ui/core';
+import { Button, Box, TextField } from '@material-ui/core';
 
 import { signupUser } from '../../services/user';
 import withValidation from '../higher-order/withValidation';
 
-function SignUp({ renderInput, handleSubmit }) {
+function SignUp({ credentials, validate, handleSubmit }) {
+   // render input (email or password)
+   const renderInput = ({ type, name }) => {
+      return (
+         <div>
+            <TextField
+               type={type}
+               id={name}
+               name={name}
+               value={credentials[name]}
+               variant="filled"
+               onChange={validate}
+               label={name}
+               error={credentials.errors[name]}
+               helperText={credentials.errors[name]}
+            />
+         </div>
+      );
+   };
+
    return (
       <Box
          display="flex"
@@ -25,12 +44,7 @@ function SignUp({ renderInput, handleSubmit }) {
             height="75%"
          >
             <h1>Sign Up</h1>
-            <Box
-               display="flex"
-               flexDirection="column"
-               justifyContent="space-between"
-               height="40%"
-            >
+            <Box display="flex" flexDirection="row">
                {renderInput({ type: 'text', name: 'email' })}
                {renderInput({ type: 'password', name: 'password' })}
                <Button
@@ -42,6 +56,7 @@ function SignUp({ renderInput, handleSubmit }) {
                   Sign up
                </Button>
             </Box>
+
             <p>
                Already registered ? <Link to="/login">Log In</Link>
             </p>
