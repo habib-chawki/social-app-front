2099c53b8bf7019638ab3db424a2a78601904176
habib-chawki <touati.habib.chawki@gmail.com>
2020-10-11 11:25:35 +0200

    Refactor: login form

--- src/components/custom/LogIn.jsx
+++ src/components/custom/LogIn.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { TextField } from '@material-ui/core';
+
 import { loginUser } from '../../services/user';
 import withValidation from '../higher-order/withValidation';
 
-function LogIn({ renderInput, handleSubmit }) {
+function LogIn({ credentials, validate, handleSubmit }) {
+   // render input (email or password)
+   const renderInput = ({ type, name }) => {
+      return (
+         <div>
+            <TextField
+               type={type}
+               id={name}
+               name={name}
+               value={credentials[name]}
+               variant="filled"
+               onChange={validate}
+               label={name}
+               error={credentials.errors[name]}
+               helperText={credentials.errors[name]}
+            />
+         </div>
+      );
+   };
    return (
       <div>
          <h1>Log In</h1>
