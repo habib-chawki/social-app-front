9288e139523066cec7d80fe47658158355e1bcaf
habib-chawki <touati.habib.chawki@gmail.com>
2020-10-12 11:31:14 +0200

    Refactor: withValidation

--- src/components/higher-order/withValidation.jsx
+++ src/components/higher-order/withValidation.jsx
@@ -1,13 +1,9 @@
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
 
 import isEmail from 'validator/lib/isEmail';
 
-function withValidation(Component, submitForm) {
+function withValidation(Component) {
    return (props) => {
-      // history object
-      const history = useHistory();
-
       // manage email, password and validation errors state
       const [credentials, setCredentials] = useState({
          email: '',
@@ -45,30 +41,8 @@ function withValidation(Component, submitForm) {
          });
       };
 
-      // handle form submission
-      const handleSubmit = async (event) => {
-         // prevent default form submission behavior
-         event.preventDefault();
-
-         // reject login in case of invalid credentials (errors object is not empty)
-         if (Object.keys(credentials.errors).length === 0) {
-            // handle user login / signup
-            await submitForm(credentials);
-
-            // navigate user to posts page
-            history.replace('/posts');
-         } else {
-            console.log('Invalid credentials.');
-         }
-      };
-
       return (
-         <Component
-            {...props}
-            credentials={credentials}
-            validate={validate}
-            handleSubmit={handleSubmit}
-         />
+         <Component {...props} credentials={credentials} validate={validate} />
       );
    };
 }
