291a552f894031daa57f1217b6f77bcedd473f2b
habib-chawki <touati.habib.chawki@gmail.com>
2020-10-18 13:20:34 +0200

    Refactor login form

--- src/components/custom/LogIn.jsx
+++ src/components/custom/LogIn.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { TextField } from '@material-ui/core';
+import Input from '../common/Input';
 
 import { loginUser } from '../../services/user';
 
@@ -9,30 +9,22 @@ import withValidation from '../higher-order/withValidation';
 import withSubmission from '../higher-order/withSubmission';
 
 function LogIn({ credentials, onValidate, onSubmit }) {
-   // render input (email or password)
-   const renderInput = ({ type, name }) => {
-      return (
-         <div>
-            <TextField
-               type={type}
-               id={name}
-               name={name}
-               value={credentials[name]}
-               variant="filled"
-               onChange={onValidate}
-               label={name}
-               error={credentials.errors[name]}
-               helperText={credentials.errors[name]}
-            />
-         </div>
-      );
-   };
    return (
       <div>
          <h1>Log In</h1>
          <form onSubmit={onSubmit}>
-            {renderInput({ type: 'text', name: 'email' })}
-            {renderInput({ type: 'password', name: 'password' })}
+            <Input
+               type="text"
+               name="email"
+               credentials={credentials}
+               onValidate={onValidate}
+            />
+            <Input
+               type="password"
+               name="password"
+               credentials={credentials}
+               onValidate={onValidate}
+            />
             <input type="submit" value="Log in" />
          </form>
          <p>
