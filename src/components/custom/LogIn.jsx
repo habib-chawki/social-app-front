8ef61f6491c2c99d687b1b4113754aed2b3e3d13
habib-chawki <touati.habib.chawki@gmail.com>
2020-10-18 12:56:52 +0200

    Handle form validation and submission

--- src/components/custom/LogIn.jsx
+++ src/components/custom/LogIn.jsx
@@ -1,32 +1,14 @@
 import React from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import { TextField } from '@material-ui/core';
 
 import { loginUser } from '../../services/user';
+
 import withValidation from '../higher-order/withValidation';
+import withSubmission from '../higher-order/withSubmission';
 
-function LogIn({ credentials, validate }) {
-   // history object
-   const history = useHistory();
-
-   // handle form submission
-   const handleSubmit = async (event) => {
-      // prevent default form submission behavior
-      event.preventDefault();
-
-      // reject login in case of invalid credentials (errors object is not empty)
-      if (Object.keys(credentials.errors).length === 0) {
-         // handle user login / signup
-         await loginUser(credentials);
-
-         // navigate user to posts page
-         history.replace('/posts');
-      } else {
-         console.log('Invalid credentials.');
-      }
-   };
-
+function LogIn({ credentials, onValidate, onSubmit }) {
    // render input (email or password)
    const renderInput = ({ type, name }) => {
       return (
@@ -37,7 +19,7 @@ function LogIn({ credentials, validate }) {
                name={name}
                value={credentials[name]}
                variant="filled"
-               onChange={validate}
+               onChange={onValidate}
                label={name}
                error={credentials.errors[name]}
                helperText={credentials.errors[name]}
@@ -48,7 +30,7 @@ function LogIn({ credentials, validate }) {
    return (
       <div>
          <h1>Log In</h1>
-         <form onSubmit={handleSubmit}>
+         <form onSubmit={onSubmit}>
             {renderInput({ type: 'text', name: 'email' })}
             {renderInput({ type: 'password', name: 'password' })}
             <input type="submit" value="Log in" />
@@ -60,4 +42,4 @@ function LogIn({ credentials, validate }) {
    );
 }
 
-export default withValidation(LogIn, loginUser);
+export default withSubmission(withValidation(LogIn), loginUser);
