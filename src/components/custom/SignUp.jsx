0f46de7c3cdf294268bdbdd3cdef09a61a13c8bf
habib-chawki <touati.habib.chawki@gmail.com>
2020-10-20 13:43:52 +0200

    Refactor: form layout

--- src/components/custom/SignUp.jsx
+++ src/components/custom/SignUp.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { Button, Box } from '@material-ui/core';
+import { Button } from '@material-ui/core';
+
 import Input from '../common/Input';
+import Form from '../common/Form';
 
 import { signupUser } from '../../services/user';
 
@@ -11,53 +13,36 @@ import withSubmission from '../higher-order/withSubmission';
 
 function SignUp({ credentials, onValidate, onSubmit }) {
    return (
-      <Box
-         display="flex"
-         flexDirection="row"
-         justifyContent="center"
-         alignItems="center"
-         height="80vh"
-      >
-         <Box
-            display="flex"
-            flexDirection="column"
-            justifyContent="space-around"
-            alignItems="center"
-            bgcolor="lightgray"
-            width="25%"
-            height="70%"
-            padding={5}
+      <Form>
+         <h1>Sign Up</h1>
+         <Input
+            type="text"
+            name="email"
+            credentials={credentials}
+            onValidate={onValidate}
+         />
+         <Input
+            type="password"
+            name="password"
+            credentials={credentials}
+            onValidate={onValidate}
+         />
+         <Button
+            fullWidth
+            type="submit"
+            variant="contained"
+            color="primary"
+            onClick={onSubmit}
          >
-            <h1>Sign Up</h1>
-            <Input
-               type="text"
-               name="email"
-               credentials={credentials}
-               onValidate={onValidate}
-            />
-            <Input
-               type="password"
-               name="password"
-               credentials={credentials}
-               onValidate={onValidate}
-            />
-            <Button
-               fullWidth
-               type="submit"
-               variant="contained"
-               color="primary"
-               onClick={onSubmit}
-            >
-               Sign up
+            Sign up
+         </Button>
+         <p>
+            Already registered ?
+            <Button component={Link} to="/login" color="primary">
+               Log In
             </Button>
-            <p>
-               Already registered ?
-               <Button component={Link} to="/login" color="primary">
-                  Log In
-               </Button>
-            </p>
-         </Box>
-      </Box>
+         </p>
+      </Form>
    );
 }
 
