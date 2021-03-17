cd17c14e4efb33e2156acddf2d56c1c44c74982a
habib-chawki <touati.habib.chawki@gmail.com>
2021-03-17 20:48:34 +0100

    Store user id in localStorage

--- src/services/storage.js
+++ src/services/storage.js
@@ -1,18 +1,24 @@
-const key = 'Token';
+const TOKEN_KEY = 'Token';
+const USER_KEY = 'User';
 
 // retrieve token from localStorage
 function getToken() {
-   return 'Bearer ' + localStorage.getItem(key);
+   return 'Bearer ' + localStorage.getItem(TOKEN_KEY);
 }
 
-// save token in localStorage
+// store token in localStorage
 function storeToken(token) {
-   localStorage.setItem(key, token);
+   localStorage.setItem(TOKEN_KEY, token);
+}
+
+// store user id in localStorage
+function storeUser(id) {
+   localStorage.setItem(USER_KEY, id);
 }
 
 // remove token from localStorage
 function removeToken() {
-   localStorage.removeItem(key);
+   localStorage.removeItem(TOKEN_KEY);
 }
 
-export { storeToken, getToken, removeToken };
+export { storeToken, storeUser, getToken, removeToken };
