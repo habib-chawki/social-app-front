2cc02cc980b1ab0ac8c5d882c3bdbf3eb7d88b53
habib-chawki <touati.habib.chawki@gmail.com>
2021-05-20 15:02:50 +0200

    Fix form layout

--- src/components/custom/ProfileForm.jsx
+++ src/components/custom/ProfileForm.jsx
@@ -5,6 +5,7 @@ import {
    MenuItem,
    FormControl,
    InputLabel,
+   Box,
 } from '@material-ui/core';
 
 function ProfileForm() {
@@ -16,19 +17,21 @@ function ProfileForm() {
 
    return (
       <form>
-         <TextField label="First name" variant="filled" />
-         <TextField label="Middle name" variant="filled" />
-         <TextField label="Last name" variant="filled" />
-         <TextField label="Address" variant="filled" />
+         <Box display="flex" flexDirection="column">
+            <TextField label="First name" variant="filled" />
+            <TextField label="Middle name" variant="filled" />
+            <TextField label="Last name" variant="filled" />
+            <TextField label="Address" variant="filled" />
 
-         <FormControl>
-            <InputLabel>Gender</InputLabel>
-            <Select value={gender} onChange={handleGenderChange}>
-               <MenuItem value="Male">Male</MenuItem>
-               <MenuItem value="Female">Female</MenuItem>
-               <MenuItem value="Other">Other</MenuItem>
-            </Select>
-         </FormControl>
+            <FormControl>
+               <InputLabel>Gender</InputLabel>
+               <Select value={gender} onChange={handleGenderChange}>
+                  <MenuItem value="Male">Male</MenuItem>
+                  <MenuItem value="Female">Female</MenuItem>
+                  <MenuItem value="Other">Other</MenuItem>
+               </Select>
+            </FormControl>
+         </Box>
       </form>
    );
 }
