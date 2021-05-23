756013f68f2f933d95d37fd0157939f33ed09fd7
habib-chawki <touati.habib.chawki@gmail.com>
2021-05-23 14:08:46 +0200

    Add Experience component

--- src/components/custom/Experience.jsx
+++ src/components/custom/Experience.jsx
@@ -1,7 +1,41 @@
 import React, { useState } from 'react';
 
+import { TextField, Button, Box } from '@material-ui/core';
+
+import {
+   KeyboardDatePicker,
+   MuiPickersUtilsProvider,
+} from '@material-ui/pickers';
+
+import DateFnsUtils from '@date-io/date-fns';
+
 function Experience() {
-   return <div>Experience</div>;
+   return (
+      <Box display="flex" flexDirection="column">
+         <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <KeyboardDatePicker
+               disableToolbar
+               variant="inline"
+               label="Start date"
+               format="dd/MM/yyyy"
+            />
+         </MuiPickersUtilsProvider>
+
+         <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <KeyboardDatePicker
+               disableToolbar
+               variant="inline"
+               label="End date"
+               format="dd/MM/yyyy"
+            />
+         </MuiPickersUtilsProvider>
+
+         <TextField label="position" variant="outlined" />
+         <TextField label="company" variant="outlined" />
+         <TextField label="description" multiline rows={6} variant="outlined" />
+         <Button>Add</Button>
+      </Box>
+   );
 }
 
 export default Experience;
