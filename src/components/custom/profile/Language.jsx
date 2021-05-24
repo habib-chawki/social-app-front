cca088b24f855701b1ead959e663204929b78dc2
habib-chawki <touati.habib.chawki@gmail.com>
2021-05-24 15:32:06 +0200

    Add Language component

---
+++ src/components/custom/profile/Language.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+import { TextField, Button, Chip, Paper, Box } from '@material-ui/core';
+
+function Language({ languages, onAddLanguage, onRemoveLanguage }) {
+   const [language, setLanguage] = useState('');
+
+   const handleLanguageChange = (event) => {
+      setLanguage(event.target.value);
+   };
+
+   /** Add the language chip only when the input field is not empty 
+             and the Add button or the Enter key is pressed */
+   const handleAddLanguage = (event) => {
+      if (
+         language !== '' &&
+         (event.type === 'click' ||
+            (event.type === 'keypress' && event.key === 'Enter'))
+      ) {
+         // add language to the list of languages, notify parent
+         onAddLanguage({ key: languages.length, label: language });
+
+         // clear text field
+         setLanguage('');
+      }
+   };
+
+   return (
+      <Box>
+         <TextField
+            onKeyPress={handleAddLanguage}
+            value={language}
+            onChange={handleLanguageChange}
+            label="Language"
+            variant="outlined"
+         />
+         <Button onClick={handleAddLanguage}>Add language</Button>
+
+         <Paper component="ul">
+            {languages.map((language) => (
+               <Chip
+                  key={language.key}
+                  label={language.label}
+                  onDelete={() => onRemoveLanguage(language)}
+               ></Chip>
+            ))}
+         </Paper>
+      </Box>
+   );
+}
+
+export default Language;
