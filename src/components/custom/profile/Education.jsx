9e0fd858411ab6b8c243a98d36464e9b7467e7bc
habib-chawki <touati.habib.chawki@gmail.com>
2021-05-24 13:54:28 +0200

    Return Education form

--- src/components/custom/profile/Education.jsx
+++ src/components/custom/profile/Education.jsx
@@ -1,4 +1,14 @@
 import React, { useState } from 'react';
+import moment from 'moment';
+
+import { TextField, Button, Box, Card } from '@material-ui/core';
+
+import {
+   KeyboardDatePicker,
+   MuiPickersUtilsProvider,
+} from '@material-ui/pickers';
+
+import DateFnsUtils from '@date-io/date-fns';
 
 function Education() {
    const [educationList, setEducationList] = useState([]);
@@ -40,6 +50,71 @@ function Education() {
       console.log(`Experience added ${JSON.stringify(education)}`);
       setEducationList([...educationList, education]);
    };
+
+   return (
+      <Box>
+         <Box display="flex" flexDirection="column">
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+               <KeyboardDatePicker
+                  value={startDate}
+                  onChange={handleStartDateChange}
+                  disableToolbar
+                  variant="inline"
+                  label="Start date"
+                  format="dd/MM/yyyy"
+               />
+            </MuiPickersUtilsProvider>
+
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+               <KeyboardDatePicker
+                  value={endDate}
+                  onChange={handleEndDateChange}
+                  disableToolbar
+                  variant="inline"
+                  label="End date"
+                  format="dd/MM/yyyy"
+               />
+            </MuiPickersUtilsProvider>
+
+            <TextField
+               value={major}
+               onChange={handleMajorChange}
+               label="major"
+               variant="outlined"
+            />
+            <TextField
+               value={school}
+               onChange={handleSchoolChange}
+               label="school"
+               variant="outlined"
+            />
+            <TextField
+               value={description}
+               onChange={handleDescriptionChange}
+               label="description"
+               multiline
+               rows={6}
+               variant="outlined"
+            />
+            <Button onClick={addEducation}>Add</Button>
+         </Box>
+
+         <Box>
+            {educationList.map((education) => (
+               <Card key={education.major}>
+                  <h2>
+                     {education.major} - {education.school}
+                  </h2>
+                  <h4>
+                     {moment(education.startDate).format('MMM YYYY')} -{' '}
+                     {moment(education.endDate).format('MMM YYYY')}
+                  </h4>
+                  <p>{education.description}</p>
+               </Card>
+            ))}
+         </Box>
+      </Box>
+   );
 }
 
 export default Education;
