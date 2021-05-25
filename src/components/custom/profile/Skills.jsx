d0cf76c86edbcf691deb860801bc903a8f3d0831
habib-chawki <touati.habib.chawki@gmail.com>
2021-05-25 13:44:27 +0200

    Render Skills component

--- src/components/custom/profile/Skills.jsx
+++ src/components/custom/profile/Skills.jsx
@@ -1,5 +1,14 @@
 import React, { useState } from 'react';
 
-function Skills() {}
+import { Box, TextField, Button } from '@material-ui/core';
+
+function Skills() {
+   return (
+      <Box display="flex" flexDirection="row">
+         <TextField label="Skill" flex="1" />
+         <Button>Add skill</Button>
+      </Box>
+   );
+}
 
 export default Skills;
