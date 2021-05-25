e83ad2aa9456f6fb7ca548992d5169cc4cc5e81d
habib-chawki <touati.habib.chawki@gmail.com>
2021-05-25 13:47:21 +0200

    Handle skill input change

--- src/components/custom/profile/Skills.jsx
+++ src/components/custom/profile/Skills.jsx
@@ -3,9 +3,20 @@ import React, { useState } from 'react';
 import { Box, TextField, Button } from '@material-ui/core';
 
 function Skills() {
+   const [skill, setSkill] = useState('');
+
+   const handleSkillChange = (event) => {
+      setSkill(event.target.value);
+   };
+
    return (
       <Box display="flex" flexDirection="row">
-         <TextField label="Skill" flex="1" />
+         <TextField
+            value={skill}
+            onChange={handleSkillChange}
+            label="Skill"
+            flex="1"
+         />
          <Button>Add skill</Button>
       </Box>
    );
