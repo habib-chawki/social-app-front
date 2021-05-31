5707396fb30d39539cb85a40bc460ebfb92b2682
habib-chawki <touati.habib.chawki@gmail.com>
2021-05-31 13:34:04 +0200

    Fetch user profile by id, when component first mounts

--- src/components/custom/profile/Profile.jsx
+++ src/components/custom/profile/Profile.jsx
@@ -1,12 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { fetchProfile } from '../../../services/profile';
 
 function Profile() {
+   const [profile, setProfile] = useState({});
+
    //extract user id
    const { userId } = useParams();
 
    useEffect(() => {
       // TODO: fetch profile by user id
+      fetchProfile(userId).then((profile) => {
+         setProfile(profile);
+      });
    }, []);
    return (
       <div>
