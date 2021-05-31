ea673ef640808a68b88041fcf8306d00e59efb16
habib-chawki <touati.habib.chawki@gmail.com>
2021-05-31 13:00:36 +0200

    Extract user id, fetch profile

--- src/components/custom/profile/Profile.jsx
+++ src/components/custom/profile/Profile.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 function Profile() {
+   //extract user id
+   const { userId } = useParams();
+
+   useEffect(() => {
+      // TODO: fetch profile by user id
+   }, []);
    return (
       <div>
          <p>Profile</p>
