30357903d65c5f73a9ed27bc85030e75c8371f88
habib-chawki <touati.habib.chawki@gmail.com>
2021-05-31 14:08:10 +0200

    Fix link to profile, add userId param

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 
 import { logoutUser } from '../../services/user';
+import { getUser } from '../../services/storage';
 
 function Header() {
+   const userId = useRef();
+
+   useEffect(() => {
+      // extract user id from local storage
+      userId.current = getUser();
+   }, []);
+
    return (
       <nav>
          <ul>
@@ -13,7 +21,7 @@ function Header() {
                </Link>
             </li>
             <li>
-               <Link to="/profile">View profile</Link>
+               <Link to={`/profile/${userId.current}`}>View profile</Link>
             </li>
          </ul>
       </nav>
