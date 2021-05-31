406d842d35b328d58ccb9d37dc8226e6b9d9db76
habib-chawki <touati.habib.chawki@gmail.com>
2021-05-31 14:42:31 +0200

    Determine whether user can update the profile

--- src/components/custom/profile/Profile.jsx
+++ src/components/custom/profile/Profile.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import { fetchProfile } from '../../../services/profile';
+import { getUser } from '../../../services/storage';
 
 import { Button } from '@material-ui/core';
 
 function Profile() {
    const history = useHistory();
    const [profile, setProfile] = useState({});
+   const [allowUpdate, setAllowUpdate] = useState(false);
 
    // extract user id
    const { userId } = useParams();
@@ -14,7 +16,11 @@ function Profile() {
    useEffect(() => {
       // fetch profile by user id
       fetchProfile(userId).then((profile) => {
+         // set the user profile
          setProfile(profile);
+
+         // determine whether the logged in user can update the profile
+         setAllowUpdate(userId === getUser());
       });
    }, [userId]);
 
@@ -24,7 +30,9 @@ function Profile() {
 
    return (
       <div>
-         <Button onClick={handleUpdateProfile}>Update profile</Button>
+         {allowUpdate && (
+            <Button onClick={handleUpdateProfile}>Update profile</Button>
+         )}
          <h1>Profile</h1>
          {JSON.stringify(profile)}
       </div>
