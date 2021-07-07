b21819771169296f2dc8dc5761f8cd5a738918ae
habib-chawki <touati.habib.chawki@gmail.com>
2021-07-07 15:28:29 +0200

    Fix: user profile link

--- src/components/custom/Post.jsx
+++ src/components/custom/Post.jsx
@@ -5,11 +5,13 @@ import Comments from './Comments';
 import withEdit from '../higher-order/withEdit';
 
 function Post(post) {
+   // extract post owner full name
    const { firstName, middleName, lastName } = post.owner.profile;
    const postOwnerFullName = `${firstName} ${middleName} ${lastName}`;
+
    return (
       <div>
-         <Link to={`profile/${post.owner._id}`}>{postOwnerFullName}</Link>
+         <Link to={`user/${post.owner._id}/profile`}>{postOwnerFullName}</Link>
 
          {post.renderContent()}
 
