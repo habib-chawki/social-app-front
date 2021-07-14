9df0c5efbdf460eaeb7b17bc66d557e9b397046a
habib-chawki <touati.habib.chawki@gmail.com>
2021-07-14 13:26:02 +0200

    Determine if users can update/delete comments

--- src/components/custom/Comment.jsx
+++ src/components/custom/Comment.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import withEdit from '../higher-order/withEdit';
 
 import { getUser } from '../../services/storage';
 
 function Comment(comment) {
+   const canEdit = useRef(getUser() === comment.owner);
+
    return (
       <li key={comment.id}>
          <h4>{comment.owner}</h4>
 
          {comment.renderContent()}
 
-         {getUser() === comment.owner && (
+         {canEdit.current && (
             <button onClick={() => comment.handleRemove()}>delete</button>
          )}
-         {getUser() === comment.owner && (
+         {canEdit.current && (
             <button onClick={() => comment.handleUpdate()}>update</button>
          )}
       </li>
