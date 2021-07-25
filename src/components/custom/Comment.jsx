2d52a04c33fa46be82fa223492aa9d3e6f0c58c0
habib-chawki <touati.habib.chawki@gmail.com>
2021-07-25 16:38:11 +0200

    Render comment Card

--- src/components/custom/Comment.jsx
+++ src/components/custom/Comment.jsx
@@ -1,12 +1,20 @@
 import React, { useState, useRef } from 'react';
+import { Link } from 'react-router-dom';
 
 import moment from 'moment';
 
 import withEdit from '../higher-order/withEdit';
-
 import { getUser } from '../../services/storage';
 
-import { IconButton, Menu, MenuItem } from '@material-ui/core';
+import {
+   Card,
+   CardContent,
+   CardHeader,
+   Avatar,
+   IconButton,
+   Menu,
+   MenuItem,
+} from '@material-ui/core';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 function Comment(comment) {
@@ -23,32 +31,42 @@ function Comment(comment) {
 
    return (
       <li key={comment.id}>
-         <h4>{comment.owner}</h4>
-         <p>{moment(comment.creationTime).format('LLL')}</p>
+         <Card>
+            <CardHeader
+               avatar={<Avatar />}
+               title={
+                  <Link to={`user/${comment.owner}/profile`}>
+                     {comment.owner}
+                  </Link>
+               }
+               subheader={moment(comment.creationTime).format('LLL')}
+               action={
+                  canEdit.current && (
+                     <div>
+                        <IconButton onClick={handleMenuClick}>
+                           <MoreVertIcon />
+                        </IconButton>
+                        <Menu
+                           id="menu"
+                           anchorEl={anchorEl}
+                           keepMounted
+                           open={Boolean(anchorEl)}
+                           onClose={handleMenuClose}
+                        >
+                           <MenuItem onClick={() => comment.handleUpdate()}>
+                              Edit
+                           </MenuItem>
+                           <MenuItem onClick={() => comment.handleRemove()}>
+                              Delete
+                           </MenuItem>
+                        </Menu>
+                     </div>
+                  )
+               }
+            />
 
-         {comment.renderContent()}
-
-         {canEdit.current && (
-            <div>
-               <IconButton onClick={handleMenuClick}>
-                  <MoreVertIcon />
-               </IconButton>
-               <Menu
-                  id="menu"
-                  anchorEl={anchorEl}
-                  keepMounted
-                  open={Boolean(anchorEl)}
-                  onClose={handleMenuClose}
-               >
-                  <MenuItem onClick={() => comment.handleUpdate()}>
-                     Edit
-                  </MenuItem>
-                  <MenuItem onClick={() => comment.handleRemove()}>
-                     Delete
-                  </MenuItem>
-               </Menu>
-            </div>
-         )}
+            <CardContent>{comment.renderContent()}</CardContent>
+         </Card>
       </li>
    );
 }
