7758af2fa9089d04514858495674b17a452271c3
habib-chawki <touati.habib.chawki@gmail.com>
2021-07-30 22:07:22 +0200

    Wrap form with container

--- src/components/common/Form.jsx
+++ src/components/common/Form.jsx
@@ -1,33 +1,9 @@
 import React from 'react';
 
-import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 
 function Form({ children }) {
-   return (
-      <Container>
-         <Grid
-            container
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-            height="80vh"
-         >
-            <Grid
-               container
-               direction="column"
-               justifyContent="space-around"
-               alignItems="center"
-               bgcolor="lightgray"
-               width="25%"
-               height="70%"
-               padding={5}
-            >
-               {children}
-            </Grid>
-         </Grid>
-      </Container>
-   );
+   return <Container>{children}</Container>;
 }
 
 export default Form;
