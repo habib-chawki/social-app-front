328d36b21484b895f640d5463753070fbf1ddaba
habib-chawki <touati.habib.chawki@gmail.com>
2021-07-31 13:28:24 +0200

    Fix items alignment and spacing

--- src/components/common/Form.jsx
+++ src/components/common/Form.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 
 import Grid from '@material-ui/core/Grid';
-import Container from '@material-ui/core/Container';
 import Paper from '@material-ui/core/Paper';
 
 function Form({ children }) {
    return (
       <Paper>
-         <Container>
-            <Grid container direction="column" spacing={4} alignItems="center">
-               {children}
-            </Grid>
-         </Container>
+         <Grid container direction="column" alignItems="center" spacing={3}>
+            {children}
+         </Grid>
       </Paper>
    );
 }
