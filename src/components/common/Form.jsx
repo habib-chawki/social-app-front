3fb4b94c2d3cb2a47e883c71d6441258901eabc0
habib-chawki <touati.habib.chawki@gmail.com>
2021-07-31 12:49:34 +0200

    Align form items

--- src/components/common/Form.jsx
+++ src/components/common/Form.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
-import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
 
 function Form({ children }) {
    return (
-      <Container>
-         <Grid container spacing={4} direction="column">
-            {children}
-         </Grid>
-      </Container>
+      <Paper>
+         <Container>
+            <Grid container direction="column" spacing={4} alignItems="center">
+               {children}
+            </Grid>
+         </Container>
+      </Paper>
    );
 }
 
