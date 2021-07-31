10b7c4e496e7eb43cc99ae94f7aaa45c9ca3ae61
habib-chawki <touati.habib.chawki@gmail.com>
2021-07-31 14:22:20 +0200

    Set form styles

--- src/components/common/Form.jsx
+++ src/components/common/Form.jsx
@@ -4,15 +4,26 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
 
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+   paper: {
+      width: 400,
+      padding: 20,
+   },
+});
+
 function Form({ children }) {
+   const classes = useStyles();
+
    return (
       <Box
          display="flex"
-         height="100vh"
+         height="95vh"
          justifyContent="center"
          alignItems="center"
       >
-         <Paper elevation={4}>
+         <Paper className={classes.paper} elevation={4}>
             <Grid container direction="column" alignItems="center" spacing={3}>
                {children}
             </Grid>
