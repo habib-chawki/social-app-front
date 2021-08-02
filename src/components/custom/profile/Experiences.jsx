06c831fbf09058e0d32f48721a68615e9f6bb5ae
habib-chawki <touati.habib.chawki@gmail.com>
2021-08-02 19:20:29 +0200

    Fix experiences cards typography

--- src/components/custom/profile/Experiences.jsx
+++ src/components/custom/profile/Experiences.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Box from '@material-ui/core/Box';
 import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
 
 import moment from 'moment';
 
@@ -10,14 +11,15 @@ function Experiences({ experiences }) {
       <Box>
          {experiences.map((experience) => (
             <Card key={experience.position}>
-               <h2>
+               <Typography variant="h4">
                   {experience.position} - {experience.company}
-               </h2>
-               <h4>
-                  {moment(experience.startDate).format('MMM YYYY')} -{' '}
+               </Typography>
+               <Typography variant="subtitle1">
+                  {moment(experience.startDate).format('MMM YYYY')} -
                   {moment(experience.endDate).format('MMM YYYY')}
-               </h4>
-               <p>{experience.description}</p>
+               </Typography>
+
+               <Typography variant="body1">{experience.description}</Typography>
             </Card>
          ))}
       </Box>
