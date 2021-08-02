eb14f949798e4e95dc1599225b48d2cccf0b3ba7
habib-chawki <touati.habib.chawki@gmail.com>
2021-08-02 19:10:04 +0200

    Render list of experiences

--- src/components/custom/profile/Experiences.jsx
+++ src/components/custom/profile/Experiences.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
 
-function Experiences() {
-   return <div>Experiences</div>;
+import Box from '@material-ui/core/Box';
+import Card from '@material-ui/core/Card';
+
+import moment from 'moment';
+
+function Experiences({ experiences }) {
+   return (
+      <Box>
+         {experiences.map((experience) => (
+            <Card key={experience.position}>
+               <h2>
+                  {experience.position} - {experience.company}
+               </h2>
+               <h4>
+                  {moment(experience.startDate).format('MMM YYYY')} -{' '}
+                  {moment(experience.endDate).format('MMM YYYY')}
+               </h4>
+               <p>{experience.description}</p>
+            </Card>
+         ))}
+      </Box>
+   );
 }
 
 export default Experiences;
