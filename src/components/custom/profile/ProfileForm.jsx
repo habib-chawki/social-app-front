c8de9422609f42dcddc84618dbd5db792413cc4f
habib-chawki <touati.habib.chawki@gmail.com>
2021-08-02 18:31:12 +0200

    Display experience dialog

--- src/components/custom/profile/ProfileForm.jsx
+++ src/components/custom/profile/ProfileForm.jsx
@@ -259,6 +259,7 @@ function ProfileForm() {
                <Dialog
                   open={openExperienceDialog}
                   onClose={() => setOpenExperienceDialog(false)}
+                  fullWidth
                >
                   <DialogTitle>Experience</DialogTitle>
                   <DialogContent>
