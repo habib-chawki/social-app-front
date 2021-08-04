cf4cada96a86b5c39e0f224f2e5afd7b7f3c6882
habib-chawki <touati.habib.chawki@gmail.com>
2021-08-04 18:28:06 +0200

    Set up education dialog

--- src/components/custom/profile/EducationDialog.jsx
+++ src/components/custom/profile/EducationDialog.jsx
@@ -1,5 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function EducationDialog() {}
+import EducationForm from './EducationForm';
+
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+
+function EducationDialog({ onAddExperience }) {
+   const [open, setOpen] = useState(false);
+
+   const openDialog = () => {
+      setOpen(true);
+   };
+
+   const closeDialog = () => {
+      setOpen(false);
+   };
+
+   return (
+      <Box>
+         <Button onClick={openDialog} fullWidth>
+            Add education
+         </Button>
+
+         <Dialog open={open} onClose={closeDialog} fullWidth>
+            <DialogTitle>Education</DialogTitle>
+            <DialogContent dividers>
+               <EducationForm
+                  onAddExperience={onAddExperience}
+                  onCloseDialog={closeDialog}
+               />
+            </DialogContent>
+         </Dialog>
+      </Box>
+   );
+}
 
 export default EducationDialog;
