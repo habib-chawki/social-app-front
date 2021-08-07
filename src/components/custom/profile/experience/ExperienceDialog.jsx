184799eac4b3be13d9ddc2a65ed5645999a56202
habib-chawki <touati.habib.chawki@gmail.com>
2021-08-07 20:44:30 +0200

    Refactor: experience dialog

--- src/components/custom/profile/experience/ExperienceDialog.jsx
+++ src/components/custom/profile/experience/ExperienceDialog.jsx
@@ -1,31 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import ExperienceForm from './ExperienceForm';
 
-import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
 
-function ExperienceDialog({ onAddExperience }) {
-   const [openExperienceDialog, setOpenExperienceDialog] = useState(false);
-
-   const onOpenExperienceDialog = () => {
-      setOpenExperienceDialog(true);
-   };
-
-   const closeDialog = () => {
-      setOpenExperienceDialog(false);
-   };
-
+function ExperienceDialog({ onAddExperience, open, closeDialog }) {
    return (
       <Box>
-         <Button onClick={onOpenExperienceDialog} fullWidth>
-            Add experience
-         </Button>
-
-         <Dialog open={openExperienceDialog} onClose={closeDialog} fullWidth>
+         <Dialog open={open} onClose={closeDialog} fullWidth>
             <DialogTitle>Experience</DialogTitle>
             <DialogContent dividers>
                <ExperienceForm
