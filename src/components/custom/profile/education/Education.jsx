d87df1f1888c1c32183667102db7b6f65d66e758
habib-chawki <touati.habib.chawki@gmail.com>
2021-08-09 11:11:13 +0200

    Move education related logic to Education component

--- src/components/custom/profile/education/Education.jsx
+++ src/components/custom/profile/education/Education.jsx
@@ -6,7 +6,52 @@ import EducationDialog from './EducationDialog';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 
-function Education() {
+function Education({ educations, setEducations }) {
+   // education dialog state
+   const [openEducationDialog, setOpenEducationDialog] = useState(false);
+   const [initialEducationFormValues, setInitialEducationFormValues] = useState(
+      {}
+   );
+
+   const handleOpenEducationDialog = (initialValues = {}) => {
+      // set the initial form values in case of an update
+      if (initialValues) {
+         setInitialEducationFormValues(initialValues);
+      }
+
+      setOpenEducationDialog(true);
+   };
+
+   const closeEducationDialog = () => {
+      setOpenEducationDialog(false);
+   };
+
+   const handleAddEducation = (education) => {
+      setEducations([...educations, education]);
+   };
+
+   const handleUpdateEducation = (education, updatedEducation) => {
+      setEducations(
+         educations.map((item) =>
+            item.major === education.major &&
+            item.school === education.school &&
+            item.startDate === education.startDate
+               ? updatedEducation
+               : item
+         )
+      );
+   };
+
+   const handleRemoveEducation = (education) => {
+      setEducations(
+         educations.filter(
+            (item) =>
+               item.major !== education.major ||
+               item.school !== education.school ||
+               item.startDate !== education.startDate
+         )
+      );
+   };
    return (
       <Box>
          <Button onClick={handleOpenEducationDialog} fullWidth>
