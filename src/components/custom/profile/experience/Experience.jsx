bf183ed982be899377dda91650283a225da11fb1
habib-chawki <touati.habib.chawki@gmail.com>
2021-08-09 10:51:22 +0200

    Handle experiences list state

--- src/components/custom/profile/experience/Experience.jsx
+++ src/components/custom/profile/experience/Experience.jsx
@@ -4,6 +4,51 @@ import Experiences from './Experiences';
 import ExperienceDialog from './ExperienceDialog';
 
 function Experience() {
+   const [experiences, setExperiences] = useState(profile.experience || []);
+   const [openExperienceDialog, setOpenExperienceDialog] = useState(false);
+   const [initialExperienceFormValues, setInitialExperienceFormValues] =
+      useState({});
+
+   const handleOpenExperienceDialog = (initialValues = {}) => {
+      // set the initial form values in case of an update
+      if (initialValues) {
+         setInitialExperienceFormValues(initialValues);
+      }
+
+      setOpenExperienceDialog(true);
+   };
+
+   const closeExperienceDialog = () => {
+      setOpenExperienceDialog(false);
+   };
+
+   const handleAddExperience = (experience) => {
+      setExperiences([...experiences, experience]);
+   };
+
+   const handleUpdateExperience = (experience, updatedExperience) => {
+      setExperiences(
+         experiences.map((item) =>
+            item.position === experience.position &&
+            item.company === experience.company &&
+            item.startDate === experience.startDate
+               ? updatedExperience
+               : item
+         )
+      );
+   };
+
+   const handleRemoveExperience = (experience) => {
+      setExperiences(
+         experiences.filter(
+            (item) =>
+               item.position !== experience.position ||
+               item.company !== experience.company ||
+               item.startDate !== experience.startDate
+         )
+      );
+   };
+
    return (
       <Box>
          <Button onClick={handleOpenExperienceDialog} fullWidth>
