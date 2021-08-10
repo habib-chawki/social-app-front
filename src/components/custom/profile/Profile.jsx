3a719041d00f393226bf41e332bea94d6926dbef
habib-chawki <touati.habib.chawki@gmail.com>
2021-08-10 12:25:15 +0200

    Add Tabs

--- src/components/custom/profile/Profile.jsx
+++ src/components/custom/profile/Profile.jsx
@@ -5,7 +5,7 @@ import { getUser } from '../../../services/storage';
 
 import moment from 'moment';
 
-import { Button, Box, Card, Chip } from '@material-ui/core';
+import { Button, Box, Card, Chip, Tabs, Tab } from '@material-ui/core';
 
 import InfoIcon from '@material-ui/icons/Info';
 import HomeIcon from '@material-ui/icons/Home';
@@ -57,127 +57,140 @@ function Profile() {
 
    return (
       <Box>
-         {
-            // determine whether the logged in user can update the profile
-            userId === getUser() && (
-               <Button onClick={handleUpdateProfile}>Update profile</Button>
-            )
-         }
-
-         {/* Basic info */}
+         <Tabs
+            variant="fullWidth"
+            indicatorColor="secondary"
+            textColor="secondary"
+         >
+            <Tab label="Bio" />
+            <Tab label="Experience" />
+            <Tab label="Education" />
+            <Tab label="Languages" />
+            <Tab label="Skills" />
+         </Tabs>
          <Box>
-            <h1>
-               <AccountBoxIcon />{' '}
-               {profile.firstName
-                  ? `${profile.firstName} ${profile.middleName} ${profile.lastName}`
-                  : 'Undetermined name'}
-            </h1>
+            {
+               // determine whether the logged in user can update the profile
+               userId === getUser() && (
+                  <Button onClick={handleUpdateProfile}>Update profile</Button>
+               )
+            }
 
-            <h2>
-               <HomeIcon /> {profile.address || 'Undetermined address'}
-            </h2>
-            <h3>
-               <CakeIcon /> {profile.birthday}
-            </h3>
-         </Box>
+            {/* Basic info */}
+            <Box>
+               <h1>
+                  <AccountBoxIcon />{' '}
+                  {profile.firstName
+                     ? `${profile.firstName} ${profile.middleName} ${profile.lastName}`
+                     : 'Undetermined name'}
+               </h1>
 
-         {/* Bio */}
-         <Box>
-            <h2>
-               <InfoIcon /> Bio
-            </h2>
-            <p>{profile.bio || 'Undetermined bio'}</p>
-         </Box>
+               <h2>
+                  <HomeIcon /> {profile.address || 'Undetermined address'}
+               </h2>
+               <h3>
+                  <CakeIcon /> {profile.birthday}
+               </h3>
+            </Box>
 
-         {/* Experience */}
-         <Box>
-            <h2>
-               <WorkIcon /> Exeperience
-            </h2>
+            {/* Bio */}
+            <Box>
+               <h2>
+                  <InfoIcon /> Bio
+               </h2>
+               <p>{profile.bio || 'Undetermined bio'}</p>
+            </Box>
 
-            {profile.experience.length === 0 ? (
-               <p>Undetermined experience</p>
-            ) : (
-               profile.experience.map((experience) => (
-                  <Card key={experience.position}>
-                     <h2>
-                        {experience.position} - {experience.company}
-                     </h2>
-                     <h4>
-                        {moment(experience.startDate).format('MMM YYYY')} -{' '}
-                        {moment(experience.endDate).format('MMM YYYY')}
-                     </h4>
-                     <p>{experience.description}</p>
-                  </Card>
-               ))
-            )}
-         </Box>
+            {/* Experience */}
+            <Box>
+               <h2>
+                  <WorkIcon /> Exeperience
+               </h2>
 
-         {/* Education */}
-         <Box>
-            <h2>
-               <SchoolIcon /> Education
-            </h2>
-            {profile.education.length === 0 ? (
-               <p>Undetermined education</p>
-            ) : (
-               profile.education.map((education) => (
-                  <Card key={education.major}>
-                     <h2>
-                        {education.major} - {education.school}
-                     </h2>
-                     <h4>
-                        {moment(education.startDate).format('MMM YYYY')} -{' '}
-                        {moment(education.endDate).format('MMM YYYY')}
-                     </h4>
-                     <p>{education.description}</p>
-                  </Card>
-               ))
-            )}
-         </Box>
-
-         {/* Languages */}
-         <Box>
-            <h2>
-               <LanguageIcon /> Languages
-            </h2>
-
-            {profile.languages.length === 0 ? (
-               <p>Undetermined languages</p>
-            ) : (
-               profile.languages.map((language, index) => (
-                  <Chip key={index} label={language}></Chip>
-               ))
-            )}
-         </Box>
-
-         {/* Skills */}
-         <Box>
-            <h2>Skills</h2>
-            <h3>
-               <WorkIcon /> Organizational
-            </h3>
-            <ul>
-               {profile.skills.organizational.length === 0 ? (
-                  <p>Undetermined organizational skills</p>
+               {profile.experience.length === 0 ? (
+                  <p>Undetermined experience</p>
                ) : (
-                  profile.skills.organizational.map((skill, index) => (
-                     <li key={index}>{skill}</li>
+                  profile.experience.map((experience) => (
+                     <Card key={experience.position}>
+                        <h2>
+                           {experience.position} - {experience.company}
+                        </h2>
+                        <h4>
+                           {moment(experience.startDate).format('MMM YYYY')} -{' '}
+                           {moment(experience.endDate).format('MMM YYYY')}
+                        </h4>
+                        <p>{experience.description}</p>
+                     </Card>
                   ))
                )}
-            </ul>
-            <h3>
-               <TimerIcon /> Technical
-            </h3>
-            <ul>
-               {profile.skills.technical.length === 0 ? (
-                  <p>Undetermined technical skills</p>
+            </Box>
+
+            {/* Education */}
+            <Box>
+               <h2>
+                  <SchoolIcon /> Education
+               </h2>
+               {profile.education.length === 0 ? (
+                  <p>Undetermined education</p>
                ) : (
-                  profile.skills.technical.map((skill, index) => (
-                     <li key={index}>{skill}</li>
+                  profile.education.map((education) => (
+                     <Card key={education.major}>
+                        <h2>
+                           {education.major} - {education.school}
+                        </h2>
+                        <h4>
+                           {moment(education.startDate).format('MMM YYYY')} -{' '}
+                           {moment(education.endDate).format('MMM YYYY')}
+                        </h4>
+                        <p>{education.description}</p>
+                     </Card>
                   ))
                )}
-            </ul>
+            </Box>
+
+            {/* Languages */}
+            <Box>
+               <h2>
+                  <LanguageIcon /> Languages
+               </h2>
+
+               {profile.languages.length === 0 ? (
+                  <p>Undetermined languages</p>
+               ) : (
+                  profile.languages.map((language, index) => (
+                     <Chip key={index} label={language}></Chip>
+                  ))
+               )}
+            </Box>
+
+            {/* Skills */}
+            <Box>
+               <h2>Skills</h2>
+               <h3>
+                  <WorkIcon /> Organizational
+               </h3>
+               <ul>
+                  {profile.skills.organizational.length === 0 ? (
+                     <p>Undetermined organizational skills</p>
+                  ) : (
+                     profile.skills.organizational.map((skill, index) => (
+                        <li key={index}>{skill}</li>
+                     ))
+                  )}
+               </ul>
+               <h3>
+                  <TimerIcon /> Technical
+               </h3>
+               <ul>
+                  {profile.skills.technical.length === 0 ? (
+                     <p>Undetermined technical skills</p>
+                  ) : (
+                     profile.skills.technical.map((skill, index) => (
+                        <li key={index}>{skill}</li>
+                     ))
+                  )}
+               </ul>
+            </Box>
          </Box>
       </Box>
    );
