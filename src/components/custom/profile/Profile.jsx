3fcac82c45745463d73bbbef734ce1c8dc1627fa
habib-chawki <touati.habib.chawki@gmail.com>
2021-08-10 14:34:01 +0200

    Set skills icon prop

--- src/components/custom/profile/Profile.jsx
+++ src/components/custom/profile/Profile.jsx
@@ -38,9 +38,6 @@ import TimerIcon from '@material-ui/icons/Timer';
 
 // lists
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
 
 // styles
 import { makeStyles } from '@material-ui/core/styles';
@@ -284,24 +281,14 @@ function Profile() {
                   <CollapsibleSkills
                      skills={profile.skills.organizational}
                      heading="Organizational"
+                     icon={<WorkIcon />}
                   />
 
-                  <ListItem button>
-                     <ListItemIcon>
-                        <TimerIcon />
-                     </ListItemIcon>
-                     <ListItemText primary="Technical" />
-                  </ListItem>
-
-                  <ul>
-                     {profile.skills.technical.length === 0 ? (
-                        <p>Undetermined technical skills</p>
-                     ) : (
-                        profile.skills.technical.map((skill, index) => (
-                           <li key={index}>{skill}</li>
-                        ))
-                     )}
-                  </ul>
+                  <CollapsibleSkills
+                     skills={profile.skills.technical}
+                     heading="Technical"
+                     icon={<TimerIcon />}
+                  />
                </List>
             )}
             {
