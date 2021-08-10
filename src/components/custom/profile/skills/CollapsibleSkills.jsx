5a4e9c5a8e05e95ba1af1dbf38dda4c34c901c5d
habib-chawki <touati.habib.chawki@gmail.com>
2021-08-10 14:29:57 +0200

    Handle skills list collapse

--- src/components/custom/profile/skills/CollapsibleSkills.jsx
+++ src/components/custom/profile/skills/CollapsibleSkills.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 
+import Box from '@material-ui/core/Box';
+import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Collapse from '@material-ui/core/Collapse';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
+
+import WorkIcon from '@material-ui/icons/Work';
 
 function CollapsibleSkills({ skills, heading }) {
    const [open, setOpen] = useState(true);
@@ -20,14 +24,14 @@ function CollapsibleSkills({ skills, heading }) {
             <ListItemIcon>
                <WorkIcon />
             </ListItemIcon>
-            <ListItemText primary="Organizational" />
+            <ListItemText primary={heading} />
             {open ? <ExpandLess /> : <ExpandMore />}
          </ListItem>
 
          <Collapse in={open}>
             <List>
                {skills.length === 0 ? (
-                  <p>Undetermined organizational skills</p>
+                  <p>Undetermined {heading} skills</p>
                ) : (
                   skills.map((skill, index) => (
                      <ListItem key={index}>
