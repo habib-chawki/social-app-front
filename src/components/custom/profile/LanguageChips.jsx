0c7f39ea4680574e6266ebc229a2bbf4f877bd67
habib-chawki <touati.habib.chawki@gmail.com>
2021-08-12 18:14:17 +0200

    Render language chips

--- src/components/custom/profile/LanguageChips.jsx
+++ src/components/custom/profile/LanguageChips.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
-function LanguageChips() {}
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import Chip from '@material-ui/core/Chip';
+
+function LanguageChips({ languages }) {
+   return (
+      <Box>
+         {languages.length === 0 ? (
+            <Typography variant="h4">Undetermined languages</Typography>
+         ) : (
+            languages.map((language, index) => (
+               <Chip key={index} label={language} color="secondary" />
+            ))
+         )}
+      </Box>
+   );
+}
 
 export default LanguageChips;
