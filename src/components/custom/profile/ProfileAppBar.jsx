63038740a5e12a2bbf6bfc01ac765885216c16af
habib-chawki <touati.habib.chawki@gmail.com>
2021-08-12 18:10:07 +0200

    Fix: render profile app bar

--- src/components/custom/profile/ProfileAppBar.jsx
+++ src/components/custom/profile/ProfileAppBar.jsx
@@ -1,5 +1,40 @@
 import React from 'react';
 
-function ProfileAppBar() {}
+// mui components
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+// mui icons
+import SchoolIcon from '@material-ui/icons/School';
+import LanguageIcon from '@material-ui/icons/Language';
+import BuildIcon from '@material-ui/icons/Build';
+import InfoIcon from '@material-ui/icons/Info';
+import WorkIcon from '@material-ui/icons/Work';
+
+function ProfileAppBar({ selectedTab, onTabChange }) {
+   return (
+      <>
+         <CssBaseline />
+
+         <AppBar position="sticky" color="default">
+            <Tabs
+               value={selectedTab}
+               onChange={onTabChange}
+               indicatorColor="secondary"
+               textColor="secondary"
+               variant="fullWidth"
+            >
+               <Tab label="Bio" icon={<InfoIcon />} />
+               <Tab label="Experience" icon={<WorkIcon />} />
+               <Tab label="Education" icon={<SchoolIcon />} />
+               <Tab label="Languages" icon={<LanguageIcon />} />
+               <Tab label="Skills" icon={<BuildIcon />} />
+            </Tabs>
+         </AppBar>
+      </>
+   );
+}
 
 export default ProfileAppBar;
