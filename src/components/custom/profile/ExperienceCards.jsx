9c009378958d32a5e3f8a227a5eeb8721205504b
habib-chawki <touati.habib.chawki@gmail.com>
2021-08-12 18:01:22 +0200

    Fix: render experience cards

--- src/components/custom/profile/ExperienceCards.jsx
+++ src/components/custom/profile/ExperienceCards.jsx
@@ -1,6 +1,29 @@
 import React from 'react';
 
+// libs
+import moment from 'moment';
+
+// mui components
+import Box from '@material-ui/core/Box';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+
+// styles
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+   paper: {
+      padding: 20,
+   },
+   paperItem: {
+      marginTop: 20,
+   },
+});
+
 function ExperienceCards({ experiences }) {
+   const classes = useStyles();
+
    return (
       <Box>
          {experiences.length === 0 ? (
