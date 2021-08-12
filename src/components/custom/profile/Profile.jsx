d5e6048e4671fde111c8ff43ef1dbd12303c23c0
habib-chawki <touati.habib.chawki@gmail.com>
2021-08-12 17:59:25 +0200

    Extract experience cards rendering logic

--- src/components/custom/profile/Profile.jsx
+++ src/components/custom/profile/Profile.jsx
@@ -238,58 +238,7 @@ function Profile() {
 
                {/* Experience */}
                {selectedTab === 1 && (
-                  <Box>
-                     {profile.experience.length === 0 ? (
-                        <p>Undetermined experience</p>
-                     ) : (
-                        profile.experience.map((experience) => (
-                           <Paper
-                              className={`${classes.paper} ${classes.paperItem}`}
-                              key={experience.position}
-                              elevation="3"
-                           >
-                              <Box
-                                 display="flex"
-                                 justifyContent="space-between"
-                                 alignItems="center"
-                              >
-                                 <Typography variant="h4">
-                                    {experience.company}
-                                 </Typography>
-
-                                 <Typography
-                                    variant="subtitle1"
-                                    color="textSecondary"
-                                 >
-                                    {moment(experience.startDate).format(
-                                       'MMMM YYYY'
-                                    )}{' '}
-                                    -{' '}
-                                    {moment(experience.endDate).format(
-                                       'MMMM YYYY'
-                                    )}
-                                 </Typography>
-                              </Box>
-
-                              <Typography
-                                 variant="h5"
-                                 className={classes.paperItem}
-                              >
-                                 {experience.position}
-                              </Typography>
-
-                              <Divider className={classes.paperItem} />
-
-                              <Typography
-                                 variant="body1"
-                                 className={classes.paperItem}
-                              >
-                                 {experience.description}
-                              </Typography>
-                           </Paper>
-                        ))
-                     )}
-                  </Box>
+                  <ExperienceCards experiences={profile.experience} />
                )}
 
                {/* Education */}
