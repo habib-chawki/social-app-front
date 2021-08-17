4a204a167b3e23698f787d52b98100da55cb5ae7
habib-chawki <touati.habib.chawki@gmail.com>
2021-08-17 17:38:40 +0200

    Handle menu actions

--- src/components/custom/PostActionsMenu.jsx
+++ src/components/custom/PostActionsMenu.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import IconButton from '@material-ui/core/IconButton';
 import Box from '@material-ui/core/Box';
@@ -11,37 +11,33 @@ import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
-function PostActionsMenu() {
-   // actions menu
-   const [anchorEl, setAnchorEl] = useState(null);
-
-   const handleCloseActionsMenu = () => {
-      setAnchorEl(null);
-   };
-
-   const handleMenuClick = (event) => {
-      setAnchorEl(event.currentTarget);
-   };
+function PostActionsMenu({
+   onMenuClick,
+   menuAnchorElement,
+   onEditAction,
+   onDeletePost,
+   closeMenu,
+}) {
    return (
       <Box>
-         <IconButton onClick={handleMenuClick}>
+         <IconButton onClick={onMenuClick}>
             <MoreHorizIcon />
          </IconButton>
 
          <Menu
             id="menu"
-            anchorEl={anchorEl}
+            anchorEl={menuAnchorElement}
             keepMounted
-            open={Boolean(anchorEl)}
-            onClose={handleCloseActionsMenu}
+            open={Boolean(menuAnchorElement)}
+            onClose={closeMenu}
          >
-            <MenuItem onClick={handleEditMenuAction}>
+            <MenuItem onClick={onEditAction}>
                <ListItemIcon>
                   <EditIcon />
                </ListItemIcon>
                <ListItemText primary="Edit" />
             </MenuItem>
-            <MenuItem onClick={handleDeletePost}>
+            <MenuItem onClick={onDeletePost}>
                <ListItemIcon>
                   <DeleteIcon />
                </ListItemIcon>
