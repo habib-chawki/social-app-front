7c605c8c53198618fef7ada8aa09b14e0dc2de29
habib-chawki <touati.habib.chawki@gmail.com>
2021-08-17 17:32:50 +0200

    Handle menu actions

--- src/components/custom/PostActionsMenu.jsx
+++ src/components/custom/PostActionsMenu.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import IconButton from '@material-ui/core/IconButton';
+import Box from '@material-ui/core/Box';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
@@ -7,29 +9,46 @@ import ListItemText from '@material-ui/core/ListItemText';
 
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 function PostActionsMenu() {
+   // actions menu
+   const [anchorEl, setAnchorEl] = useState(null);
+
+   const handleCloseActionsMenu = () => {
+      setAnchorEl(null);
+   };
+
+   const handleMenuClick = (event) => {
+      setAnchorEl(event.currentTarget);
+   };
    return (
-      <Menu
-         id="menu"
-         anchorEl={anchorEl}
-         keepMounted
-         open={Boolean(anchorEl)}
-         onClose={handleCloseActionsMenu}
-      >
-         <MenuItem onClick={handleEditMenuAction}>
-            <ListItemIcon>
-               <EditIcon />
-            </ListItemIcon>
-            <ListItemText primary="Edit" />
-         </MenuItem>
-         <MenuItem onClick={handleDeletePost}>
-            <ListItemIcon>
-               <DeleteIcon />
-            </ListItemIcon>
-            <ListItemText primary="Delete" />
-         </MenuItem>
-      </Menu>
+      <Box>
+         <IconButton onClick={handleMenuClick}>
+            <MoreHorizIcon />
+         </IconButton>
+
+         <Menu
+            id="menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleCloseActionsMenu}
+         >
+            <MenuItem onClick={handleEditMenuAction}>
+               <ListItemIcon>
+                  <EditIcon />
+               </ListItemIcon>
+               <ListItemText primary="Edit" />
+            </MenuItem>
+            <MenuItem onClick={handleDeletePost}>
+               <ListItemIcon>
+                  <DeleteIcon />
+               </ListItemIcon>
+               <ListItemText primary="Delete" />
+            </MenuItem>
+         </Menu>
+      </Box>
    );
 }
 
