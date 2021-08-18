e8875e1c872ab0f58aca0667314ed36739808c23
habib-chawki <touati.habib.chawki@gmail.com>
2021-08-18 17:17:35 +0200

    Handle update comment

--- src/components/custom/CommentEditDialog.jsx
+++ src/components/custom/CommentEditDialog.jsx
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function CommentEditDialog() {}
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogActions from '@material-ui/core/DialogActions';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+function CommentEditDialog({
+   isDialogOpen,
+   closeDialog,
+   commentId,
+   commentContent,
+   onUpdateComment,
+}) {
+   // updated comment content
+   const [updatedContent, setUpdatedContent] = useState(commentContent);
+
+   // handle input change
+   const handleUpdatedContentChange = (event) => {
+      setUpdatedContent(event.target.value);
+   };
+
+   const handleUpdateComment = () => {
+      if (updatedContent.trim() !== commentContent) {
+         onUpdateComment(commentId, updatedContent);
+      }
+
+      closeDialog();
+   };
+}
 
 export default CommentEditDialog;
