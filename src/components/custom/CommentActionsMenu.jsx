679580c5e0cddf504f4fa76e2547a04fe2dc4533
habib-chawki <touati.habib.chawki@gmail.com>
2021-08-18 17:06:07 +0200

    Handle comment actions menu

--- src/components/custom/CommentActionsMenu.jsx
+++ src/components/custom/CommentActionsMenu.jsx
@@ -1,5 +1,50 @@
 import React from 'react';
 
-function CommentActionsMenu() {}
+import Box from '@material-ui/core/Box';
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+function CommentActionsMenu({
+   onMenuClick,
+   anchorEl,
+   onMenuClose,
+   onUpdateComment,
+   onDeleteComment,
+}) {
+   return (
+      <Box>
+         <IconButton onClick={onMenuClick}>
+            <MoreVertIcon />
+         </IconButton>
+         <Menu
+            id="menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={onMenuClose}
+         >
+            <MenuItem onClick={() => onUpdateComment()}>
+               <ListItemIcon>
+                  <EditIcon />
+               </ListItemIcon>
+               <ListItemText primary="Edit" />
+            </MenuItem>
+            <MenuItem onClick={() => onDeleteComment()}>
+               <ListItemIcon>
+                  <DeleteIcon />
+               </ListItemIcon>
+               <ListItemText primary="Delete" />
+            </MenuItem>
+         </Menu>
+      </Box>
+   );
+}
 
 export default CommentActionsMenu;
