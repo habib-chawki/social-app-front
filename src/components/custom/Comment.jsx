108dfe69e1b660dd6c3ae3546ba7a8857cf8aac0
habib-chawki <touati.habib.chawki@gmail.com>
2021-08-18 17:05:55 +0200

    Move comment actions menu logic

--- src/components/custom/Comment.jsx
+++ src/components/custom/Comment.jsx
@@ -3,26 +3,21 @@ import { Link as RouterLink } from 'react-router-dom';
 
 import moment from 'moment';
 
+import CommentActionsMenu from './CommentActionsMenu';
+
 import withEdit from '../higher-order/withEdit';
 import UserContext from '../../context/user-context';
 
 // mui components
 import Box from '@material-ui/core/Box';
 import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
+
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import EditIcon from '@material-ui/icons/Edit';
-import DeleteIcon from '@material-ui/icons/Delete';
 
 function Comment(comment) {
    const loggedInUser = useContext(UserContext);
@@ -68,33 +63,7 @@ function Comment(comment) {
          <ListItemSecondaryAction>
             {
                // Determine whether user can edit comment
-               loggedInUser === comment.owner._id && (
-                  <Box>
-                     <IconButton onClick={handleMenuClick}>
-                        <MoreVertIcon />
-                     </IconButton>
-                     <Menu
-                        id="menu"
-                        anchorEl={anchorEl}
-                        keepMounted
-                        open={Boolean(anchorEl)}
-                        onClose={handleMenuClose}
-                     >
-                        <MenuItem onClick={() => comment.handleUpdate()}>
-                           <ListItemIcon>
-                              <EditIcon />
-                           </ListItemIcon>
-                           <ListItemText primary="Edit" />
-                        </MenuItem>
-                        <MenuItem onClick={() => comment.handleRemove()}>
-                           <ListItemIcon>
-                              <DeleteIcon />
-                           </ListItemIcon>
-                           <ListItemText primary="Delete" />
-                        </MenuItem>
-                     </Menu>
-                  </Box>
-               )
+               loggedInUser === comment.owner._id && <CommentActionsMenu />
             }
          </ListItemSecondaryAction>
       </ListItem>
