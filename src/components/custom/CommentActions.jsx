45a0a1ba1a440cebc302b32b284b104f15f76263
habib-chawki <touati.habib.chawki@gmail.com>
2021-08-18 17:29:56 +0200

    Add comment actions

--- src/components/custom/CommentActions.jsx
+++ src/components/custom/CommentActions.jsx
@@ -1,5 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function CommentActions() {}
+import CommentEditDialog from './CommentEditDialog';
+import CommentActionsMenu from './CommentActionsMenu';
+
+import Box from '@material-ui/core/Box';
+
+function CommentActions({
+   commentId,
+   commentContent,
+   onUpdateComment,
+   onDeleteComment,
+}) {
+   // edit comment dialog
+   const [openEditDialog, setOpenEditDialog] = useState(false);
+
+   // actions menu
+   const [anchorEl, setAnchorEl] = useState(null);
+
+   // open / close edit dialog
+   const handleOpenEditDialog = () => {
+      setOpenEditDialog(true);
+   };
+
+   const handleCloseEditDialog = () => {
+      setOpenEditDialog(false);
+   };
+
+   // actions menu
+   const handleCloseActionsMenu = () => {
+      setAnchorEl(null);
+   };
+
+   const handleMenuClick = (event) => {
+      setAnchorEl(event.currentTarget);
+   };
+
+   const handleEditMenuAction = () => {
+      // open edit dialog and close actions menu
+      handleOpenEditDialog();
+      handleCloseActionsMenu();
+   };
+
+   return (
+      <Box>
+         <CommentActionsMenu
+            onMenuClick={handleMenuClick}
+            menuAnchorElement={anchorEl}
+            onEditAction={handleEditMenuAction}
+            onDeleteComment={onDeleteComment}
+            closeMenu={handleCloseActionsMenu}
+         />
+
+         <CommentEditDialog
+            commentId={commentId}
+            commentContent={commentContent}
+            isDialogOpen={openEditDialog}
+            closeDialog={handleCloseEditDialog}
+            onUpdateComment={onUpdateComment}
+         />
+      </Box>
+   );
+}
 
 export default CommentActions;
