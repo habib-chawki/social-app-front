0ad00476bda34be228b7eae51e6d30276735e9c8
habib-chawki <touati.habib.chawki@gmail.com>
2021-08-19 18:46:58 +0200

    Fix layout

--- src/components/custom/profile/Fallback.jsx
+++ src/components/custom/profile/Fallback.jsx
@@ -7,8 +7,13 @@ import ErrorIcon from '@material-ui/icons/Error';
 
 function Fallback({ message }) {
    return (
-      <Box>
-         <ErrorIcon fontSize="medium" />
+      <Box
+         display="flex"
+         flexDirection="column"
+         justifyContent="center"
+         alignItems="center"
+      >
+         <ErrorIcon fontSize="medium" color="secondary" />
          <Typography variant="h5">{message}</Typography>
       </Box>
    );
