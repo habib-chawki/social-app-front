ea51953366394c9b158b8519ade33941901c6dbb
habib-chawki <touati.habib.chawki@gmail.com>
2021-08-19 18:50:38 +0200

    Set experience fallback

--- src/components/custom/profile/experience/ExperienceCards.jsx
+++ src/components/custom/profile/experience/ExperienceCards.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Fallback from '../Fallback';
+
 // libs
 import moment from 'moment';
 
@@ -24,42 +26,42 @@ const useStyles = makeStyles({
 function ExperienceCards({ experiences }) {
    const classes = useStyles();
 
+   if (experiences.length === 0) {
+      return <Fallback message="Experience not provided" />;
+   }
+
    return (
       <Box>
-         {experiences.length === 0 ? (
-            <p>Undetermined experience</p>
-         ) : (
-            experiences.map((experience) => (
-               <Paper
-                  className={`${classes.paper} ${classes.paperItem}`}
-                  key={experience.position}
-                  elevation="3"
+         {experiences.map((experience) => (
+            <Paper
+               className={`${classes.paper} ${classes.paperItem}`}
+               key={experience.position}
+               elevation="3"
+            >
+               <Box
+                  display="flex"
+                  justifyContent="space-between"
+                  alignItems="center"
                >
-                  <Box
-                     display="flex"
-                     justifyContent="space-between"
-                     alignItems="center"
-                  >
-                     <Typography variant="h4">{experience.company}</Typography>
+                  <Typography variant="h4">{experience.company}</Typography>
 
-                     <Typography variant="subtitle1" color="textSecondary">
-                        {moment(experience.startDate).format('MMMM YYYY')} -{' '}
-                        {moment(experience.endDate).format('MMMM YYYY')}
-                     </Typography>
-                  </Box>
-
-                  <Typography variant="h5" className={classes.paperItem}>
-                     {experience.position}
+                  <Typography variant="subtitle1" color="textSecondary">
+                     {moment(experience.startDate).format('MMMM YYYY')} -{' '}
+                     {moment(experience.endDate).format('MMMM YYYY')}
                   </Typography>
+               </Box>
 
-                  <Divider className={classes.paperItem} />
+               <Typography variant="h5" className={classes.paperItem}>
+                  {experience.position}
+               </Typography>
 
-                  <Typography variant="body1" className={classes.paperItem}>
-                     {experience.description}
-                  </Typography>
-               </Paper>
-            ))
-         )}
+               <Divider className={classes.paperItem} />
+
+               <Typography variant="body1" className={classes.paperItem}>
+                  {experience.description}
+               </Typography>
+            </Paper>
+         ))}
       </Box>
    );
 }
