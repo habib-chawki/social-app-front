43206667bad5c26a0224ad5e3c1716ad72074c84
habib-chawki <touati.habib.chawki@gmail.com>
2021-08-30 18:01:36 +0200

    Remove auth token interceptor

--- src/utils/server.js
+++ src/utils/server.js
@@ -1,14 +1,7 @@
 import axios from 'axios';
-import { getToken } from '../services/storage';
 
 const server = axios.create({
    baseURL: process.env.REACT_APP_BACKEND_BASE_URL,
-});
-
-// add auth token to request headers
-server.interceptors.request.use((config) => {
-   config.headers.authorization = getToken();
-   return config;
 });
 
 export default server;
