8bb809b6eeb77d7a084026a4220b4016f86524da
habib-chawki <touati.habib.chawki@gmail.com>
2021-08-31 17:59:03 +0200

    Protect routes, allow routing for authenticated users only

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
 
-function ProtectedRoute() {}
+import { getUser } from '../services/storage';
+
+function ProtectedRoute({ children, ...rest }) {
+   // allow routing only when user is authenticated
+   return (
+      <Route
+         {...rest}
+         render={() => (getUser() ? children : <Redirect to="/login" />)}
+      />
+   );
+}
 
 export default ProtectedRoute;
