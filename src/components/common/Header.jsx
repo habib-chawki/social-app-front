4d9a6c956732bad4a541908be2275cc9b1c6022a
habib-chawki <touati.habib.chawki@gmail.com>
2021-09-01 19:10:31 +0200

    Add home route to app bar header

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -9,6 +9,7 @@ import { IconButton, AppBar, Toolbar, Box } from '@material-ui/core';
 import Tooltip from '@material-ui/core/Tooltip';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
+import HomeIcon from '@material-ui/icons/Home';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import LockIcon from '@material-ui/icons/Lock';
 
@@ -22,6 +23,16 @@ function Header() {
          <AppBar position="static">
             <Toolbar>
                <Box display="flex" justifyContent="flex-end" width="100%">
+                  <Tooltip title="Home">
+                     <IconButton
+                        color="inherit"
+                        component={RouterLink}
+                        to="/posts"
+                     >
+                        <HomeIcon />
+                     </IconButton>
+                  </Tooltip>
+
                   <Tooltip title="View profile">
                      <IconButton
                         color="inherit"
