c2b646de4aeab1ff1d3363bf6f3a4b3f34162d3a
habib-chawki <touati.habib.chawki@gmail.com>
2021-09-01 19:45:33 +0200

    Set experience fallback component

--- src/components/custom/profile/experience/Experience.jsx
+++ src/components/custom/profile/experience/Experience.jsx
@@ -4,7 +4,12 @@ import Experiences from './Experiences';
 import ExperienceDialog from './ExperienceDialog';
 
 import Box from '@material-ui/core/Box';
+import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+
+import NoteAddIcon from '@material-ui/icons/NoteAdd';
 
 function Experience({ experiences, setExperiences }) {
    const [openDialog, setOpenDialog] = useState(false);
@@ -49,6 +54,35 @@ function Experience({ experiences, setExperiences }) {
          )
       );
    };
+
+   if (experiences.length === 0) {
+      return (
+         <Box>
+            <Paper variant="outlined">
+               <Box
+                  display="flex"
+                  flexDirection="column"
+                  justifyContent="center"
+                  alignItems="center"
+               >
+                  <Typography gutterBottom variant="h6">
+                     Add experience
+                  </Typography>
+                  <IconButton onClick={handleOpenDialog}>
+                     <NoteAddIcon />
+                  </IconButton>
+               </Box>
+            </Paper>
+            <ExperienceDialog
+               onAddExperience={handleAddExperience}
+               onUpdateExperience={handleUpdateExperience}
+               open={openDialog}
+               closeDialog={closeDialog}
+               initialFormValues={initialFormValues}
+            />
+         </Box>
+      );
+   }
 
    return (
       <Box>
