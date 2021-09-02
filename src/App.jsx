5ccca850c90d38cf5a4bc4476ba13650ccf7544b
habib-chawki <touati.habib.chawki@gmail.com>
2021-09-02 19:33:07 +0200

    Add theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
 import React from 'react';
 import Routes from './routes/Routes';
-
-import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './theme';
 
 import './index.css';
-
-const theme = createTheme({
-   palette: {
-      primary: {
-         main: '#1565c0',
-      },
-      secondary: {
-         main: '#ff6f00',
-      },
-   },
-});
 
 function App() {
    return (
