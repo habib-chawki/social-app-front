68d457730a4bf6e6f02ca1f7c3d56d7be06db8ba
habib-chawki <touati.habib.chawki@gmail.com>
2021-09-02 21:04:06 +0200

    Remove unnecessary styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import Routes from './routes/Routes';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './theme';
 
-import './index.css';
-
 function App() {
    return (
       <ThemeProvider theme={theme}>
