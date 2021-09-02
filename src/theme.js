1deacc222635eb5853040dff9ca2ddb32d35ed33
habib-chawki <touati.habib.chawki@gmail.com>
2021-09-02 19:32:58 +0200

    Add mui theme

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@material-ui/core/styles';
+
+const theme = createTheme({
+   palette: {
+      primary: {
+         main: '#1565c0',
+      },
+      secondary: {
+         main: '#7b1fa2',
+      },
+   },
+});
+
+export default theme;
