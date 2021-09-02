57af52fd9f8b80572b726cf3f4121b198492f8f6
habib-chawki <touati.habib.chawki@gmail.com>
2021-09-02 19:30:03 +0200

    Add theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import Routes from './routes/Routes';
 
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+
 import './index.css';
 
+const theme = createTheme({
+   palette: {
+      primary: {
+         main: '#1565c0',
+      },
+      secondary: {
+         main: '#ff6f00',
+      },
+   },
+});
+
 function App() {
-   return <Routes />;
+   return (
+      <ThemeProvider theme={theme}>
+         <Routes />
+      </ThemeProvider>
+   );
 }
 
 export default App;
