6d1fe0a6d45ea493f1872f2483875531a327ee8e
habib-chawki <touati.habib.chawki@gmail.com>
2021-09-03 12:40:23 +0200

    Add profile form section

---
+++ src/components/custom/profile/main/ProfileFormSection.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+   paper: {
+      padding: theme.spacing(5),
+   },
+}));
+
+function ProfileFormSection({ children }) {
+   const classes = useStyles();
+
+   return (
+      <Grid item>
+         <Paper className={classes.paper}>
+            {/* sets spacing between section elements */}
+            <Grid container direction="column" spacing={3}>
+               {children}
+            </Grid>
+         </Paper>
+      </Grid>
+   );
+}
+
+export default ProfileFormSection;
