5ad2600c8137a51952d99cad11ecba532e99a19d
habib-chawki <touati.habib.chawki@gmail.com>
2021-09-03 12:48:43 +0200

    Set profile form container

--- src/components/custom/profile/main/ProfileFormContainer.jsx
+++ src/components/custom/profile/main/ProfileFormContainer.jsx
@@ -1,5 +1,32 @@
 import React from 'react';
 
-function ProfileFormContainer() {}
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+   container: {
+      width: '55%',
+      margin: theme.spacing(3),
+   },
+}));
+
+function ProfileFormContainer({ children }) {
+   const classes = useStyles();
+
+   return (
+      <Box display="flex" justifyContent="center">
+         {/* sets spacing between form sections */}
+         <Grid
+            container
+            className={classes.container}
+            direction="column"
+            spacing={5}
+         >
+            {children}
+         </Grid>
+      </Box>
+   );
+}
 
 export default ProfileFormContainer;
