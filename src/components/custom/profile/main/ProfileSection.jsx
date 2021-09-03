7fc43ae9ad13cb538534a62a8f9f3d5ae4320c74
habib-chawki <touati.habib.chawki@gmail.com>
2021-09-03 12:39:32 +0200

    Add profile section

--- src/components/custom/profile/main/ProfileSection.jsx
+++ src/components/custom/profile/main/ProfileSection.jsx
@@ -1,5 +1,28 @@
 import React from 'react';
 
-function ProfileSection() {}
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+   paper: {
+      padding: theme.spacing(5),
+   },
+}));
+
+function ProfileSection({ children }) {
+   const classes = useStyles();
+
+   return (
+      <Grid item>
+         <Paper className={classes.paper}>
+            {/* sets spacing between section elements */}
+            <Grid container direction="column" spacing={3}>
+               {children}
+            </Grid>
+         </Paper>
+      </Grid>
+   );
+}
 
 export default ProfileSection;
